46f0225aeecafdedb5578a75c4d84e294f734970
Bill Keese <bill@dojotoolkit.org>
2013-09-10 13:38:40 +0900

    Move dijit.css rules into themes/common/common.less

--- themes/blackberry/common.js
+++ themes/blackberry/common.js
@@ -1,4 +1,90 @@
 define(function(){ return '\
+.duiReset {\
+  margin: 0;\
+  border: 0;\
+  padding: 0;\
+  font: inherit;\
+  line-height: normal;\
+  color: inherit;\
+}\
+.dj_a11y .duiReset {\
+  -moz-appearance: none;\
+}\
+.duiInline {\
+  display: inline-block;\
+  border: 0;\
+  padding: 0;\
+  vertical-align: middle;\
+}\
+table.duiInline {\
+  display: inline-table;\
+  box-sizing: content-box;\
+  -moz-box-sizing: content-box;\
+}\
+.duiHidden {\
+  display: none !important;\
+}\
+.duiVisible {\
+  display: block !important;\
+  position: relative;\
+}\
+.duiOffScreen {\
+  position: absolute !important;\
+  left: -10000px !important;\
+  top: -10000px !important;\
+}\
+.duiBackgroundIframe {\
+  position: absolute;\
+  left: 0;\
+  top: 0;\
+  width: 100%;\
+  height: 100%;\
+  z-index: -1;\
+  border: 0;\
+  padding: 0;\
+  margin: 0;\
+}\
+.duiContainer {\
+  overflow: hidden;\
+}\
+.dj_a11y .duiIcon,\
+.dj_a11y div.duiArrowButtonInner,\
+.dj_a11y span.duiArrowButtonInner,\
+.dj_a11y img.duiArrowButtonInner {\
+  display: none;\
+}\
+.dj_a11y .duiA11ySideArrow {\
+  display: inline !important;\
+  cursor: pointer;\
+}\
+.duiLayoutContainer {\
+  position: relative;\
+  display: block;\
+  overflow: hidden;\
+}\
+.duiAlignTop,\
+.duiAlignBottom,\
+.duiAlignLeft,\
+.duiAlignRight {\
+  position: absolute;\
+  overflow: hidden;\
+}\
+body .duiAlignClient {\
+  position: absolute;\
+}\
+.duiNoIcon {\
+  display: none;\
+}\
+.duiReadOnly *,\
+.duiDisabled *,\
+.duiReadOnly,\
+.duiDisabled {\
+  cursor: default;\
+}\
+.dj_gecko .duiArrowButtonInner INPUT,\
+.dj_gecko INPUT.duiArrowButtonInner {\
+  -moz-user-focus: ignore;\
+}\
 html.mobile,\
 .mobile body {\
   width: 100%;\
@@ -14,7 +100,6 @@ html.mobile,\
 .duiBackground {\
   background-color: #dedfde;\
 }\
-/* Button Colors */\
 .duiColorBlue {\
   color: #ffffff;\
   background-color: #366edf;\
@@ -25,7 +110,6 @@ html.mobile,\
   background-image: -webkit-gradient(linear, left top, right bottom, from(#7a9de9), to(#2362dd), color-stop(0.5, #366edf), color-stop(0.5, #215fdc));\
   background-image: linear-gradient(to right bottom, #7a9de9 0%, #366edf 50%, #215fdc 50%, #2362dd 100%);\
 }\
-/* Default Button Colors */\
 .duiColorDefault {\
   color: #000000;\
   background-color: #ced3ce;\
--- themes/custom/common.js
+++ themes/custom/common.js
@@ -1,4 +1,90 @@
 define(function(){ return '\
+.duiReset {\
+  margin: 0;\
+  border: 0;\
+  padding: 0;\
+  font: inherit;\
+  line-height: normal;\
+  color: inherit;\
+}\
+.dj_a11y .duiReset {\
+  -moz-appearance: none;\
+}\
+.duiInline {\
+  display: inline-block;\
+  border: 0;\
+  padding: 0;\
+  vertical-align: middle;\
+}\
+table.duiInline {\
+  display: inline-table;\
+  box-sizing: content-box;\
+  -moz-box-sizing: content-box;\
+}\
+.duiHidden {\
+  display: none !important;\
+}\
+.duiVisible {\
+  display: block !important;\
+  position: relative;\
+}\
+.duiOffScreen {\
+  position: absolute !important;\
+  left: -10000px !important;\
+  top: -10000px !important;\
+}\
+.duiBackgroundIframe {\
+  position: absolute;\
+  left: 0;\
+  top: 0;\
+  width: 100%;\
+  height: 100%;\
+  z-index: -1;\
+  border: 0;\
+  padding: 0;\
+  margin: 0;\
+}\
+.duiContainer {\
+  overflow: hidden;\
+}\
+.dj_a11y .duiIcon,\
+.dj_a11y div.duiArrowButtonInner,\
+.dj_a11y span.duiArrowButtonInner,\
+.dj_a11y img.duiArrowButtonInner {\
+  display: none;\
+}\
+.dj_a11y .duiA11ySideArrow {\
+  display: inline !important;\
+  cursor: pointer;\
+}\
+.duiLayoutContainer {\
+  position: relative;\
+  display: block;\
+  overflow: hidden;\
+}\
+.duiAlignTop,\
+.duiAlignBottom,\
+.duiAlignLeft,\
+.duiAlignRight {\
+  position: absolute;\
+  overflow: hidden;\
+}\
+body .duiAlignClient {\
+  position: absolute;\
+}\
+.duiNoIcon {\
+  display: none;\
+}\
+.duiReadOnly *,\
+.duiDisabled *,\
+.duiReadOnly,\
+.duiDisabled {\
+  cursor: default;\
+}\
+.dj_gecko .duiArrowButtonInner INPUT,\
+.dj_gecko INPUT.duiArrowButtonInner {\
+  -moz-user-focus: ignore;\
+}\
 html.mobile,\
 .mobile body {\
   width: 100%;\
@@ -15,7 +101,6 @@ html.mobile,\
 .duiBackground {\
   background-color: #c0c0c0;\
 }\
-/* Button Colors */\
 .duiColorBlue {\
   color: #ffffff;\
   background-color: #048bf4;\
@@ -26,7 +111,6 @@ html.mobile,\
   background-image: -webkit-gradient(linear, left top, right bottom, from(#048bf4), to(#48adfc));\
   background-image: linear-gradient(to right bottom, #048bf4 0%, #48adfc 100%);\
 }\
-/* Default Button Colors */\
 .duiColorDefault {\
   color: #000000;\
   background-color: #a4a4a4;\
