540007270cf07446a056d81d6856be1b0ed00a14
Adrian Vasiliu <vasiliu@fr.ibm.com>
2013-10-08 14:46:07 +0200

    Update after removal of dojox/mobile/common => dui/common. For now using dui/mobile/_base.

--- mobile/tests/bidi/doh/module.js
+++ mobile/tests/bidi/doh/module.js
@@ -1,21 +1,15 @@
-dojo.provide("dui.mobile.tests.bidi.doh.module");
-
-try{
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_EdgeToEdgeList", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_EdgeToEdgeList.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_IconContainer", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_IconContainer.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_SpinWheel-custom", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_SpinWheel-custom.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_ValuePicker-custom", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_ValuePicker-custom.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_Switch", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_Switch.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_Tooltip", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_Tooltip.html"),999999);	
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_Accordion-demo", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_Accordion-demo.html"),999999);		
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_TreeView", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_TreeView.html"),999999);		
-	doh.registerUrl("dui.mobile.tests.bidi.doh.test_RoundRect-EdgeToEdgeStoreList", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/test_RoundRect-EdgeToEdgeStoreList.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.ButtonTests", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/ButtonTests.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.HeadingTests", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/HeadingTests.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.IconMenuTests", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/IconMenuTests.html"),999999);
-	doh.registerUrl("dui.mobile.tests.bidi.doh.TabBarTests", dojo.moduleUrl("dui.mobile", "tests/bidi/doh/TabBarTests.html"),999999);	
-}catch(e){
-	doh.debug(e);
-}
-
-
+define(["doh/main", "require", "dojo/sniff"], function(doh, require){
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_EdgeToEdgeList", require.toUrl("./test_EdgeToEdgeList.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_IconContainer", require.toUrl("./test_IconContainer.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_SpinWheel-custom", require.toUrl("./test_SpinWheel-custom.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_ValuePicker-custom", require.toUrl("./test_ValuePicker-custom.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_Switch", require.toUrl("./test_Switch.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_Tooltip", require.toUrl("./test_Tooltip.html"),999999);	
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_Accordion-demo", require.toUrl("./test_Accordion-demo.html"),999999);		
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_TreeView", require.toUrl("./test_TreeView.html"),999999);		
+	doh.registerUrl("dui.mobile.tests.bidi.doh.test_RoundRect-EdgeToEdgeStoreList", require.toUrl("./test_RoundRect-EdgeToEdgeStoreList.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.ButtonTests", require.toUrl("./ButtonTests.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.HeadingTests", require.toUrl("./HeadingTests.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.IconMenuTests", require.toUrl("./IconMenuTests.html"),999999);
+	doh.registerUrl("dui.mobile.tests.bidi.doh.TabBarTests", require.toUrl("./TabBarTests.html"),999999);	
+)};
--- mobile/tests/robot/module.js
+++ mobile/tests/robot/module.js
@@ -1,29 +1,26 @@
-dojo.provide("dui.mobile.tests.robot.module");
-
-try{
+define(["doh/main", "require", "dojo/sniff"], function(doh, require){
 	var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g,"").replace(/^&/,"?"),
 		test_robot = true;
 		
 	if(test_robot){
-		doh.registerUrl("dui.mobile.tests.robot.RoundRectList", dojo.moduleUrl("dui.mobile", "tests/robot/RoundRectListConnect.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.ButtonList", dojo.moduleUrl("dui.mobile", "tests/robot/ButtonList.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.ButtonList", dojo.moduleUrl("dui.mobile", "tests/robot/ButtonList2.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.switch", dojo.moduleUrl("dui.mobile", "tests/robot/Switch.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.switch", dojo.moduleUrl("dui.mobile", "tests/robot/Switch2.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.ListItem", dojo.moduleUrl("dui.mobile", "tests/robot/ListItem.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.tabBar", dojo.moduleUrl("dui.mobile", "tests/robot/TabBar.html"), 999999);
-		doh.registerUrl("dui.mobile.tests.robot.tabBar", dojo.moduleUrl("dui.mobile", "tests/robot/TabBar2.html"), 999999);
-//		doh.registerUrl("dui.mobile.tests.robot.IconItem", dojo.moduleUrl("dui.mobile", "tests/robot/Icon.html",999999));
-//		doh.registerUrl("dui.mobile.tests.robot.IconItem", dojo.moduleUrl("dui.mobile", "tests/robot/Icon2.html",999999));
-		doh.registerUrl("dui.mobile.tests.robot.Animation", dojo.moduleUrl("dui.mobile", "tests/robot/Animation.html"),999999);
+		doh.registerUrl("dui.mobile.tests.robot.RoundRectList", require.toUrl("./RoundRectListConnect.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.ButtonList", require.toUrl("./ButtonList.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.ButtonList", require.toUrl("./ButtonList2.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.switch", require.toUrl("./Switch.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.switch", require.toUrl("./Switch2.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.ListItem", require.toUrl("./ListItem.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.tabBar", require.toUrl("./TabBar.html"), 999999);
+		doh.registerUrl("dui.mobile.tests.robot.tabBar", require.toUrl("./TabBar2.html"), 999999);
+//		doh.registerUrl("dui.mobile.tests.robot.IconItem", require.toUrl("./Icon.html",999999));
+//		doh.registerUrl("dui.mobile.tests.robot.IconItem", require.toUrl("./Icon2.html",999999));
+		doh.registerUrl("dui.mobile.tests.robot.Animation", require.toUrl("./Animation.html"),999999);
 		if(!dojo.isSafari) {
-			doh.registerUrl("dui.mobile.tests.robot.Settings", dojo.moduleUrl("dui.mobile", "tests/robot/Settings.html"),999999);
-//			doh.registerUrl("dui.mobile.tests.robot.Flippable", dojo.moduleUrl("dui.mobile", "tests/robot/Flippable.html"),999999);
-			doh.registerUrl("dui.mobile.tests.robot.Scrollable", dojo.moduleUrl("dui.mobile", "tests/robot/ScrollableView.html"),999999);
-			doh.registerUrl("dui.mobile.tests.robot.Scrollable", dojo.moduleUrl("dui.mobile", "tests/robot/ScrollableView2.html"),999999);
-			doh.registerUrl("dui.mobile.tests.robot.Scrollable", dojo.moduleUrl("dui.mobile", "tests/robot/ScrollableView3.html"),999999);
+			doh.registerUrl("dui.mobile.tests.robot.Settings", require.toUrl("./Settings.html"),999999);
+//			doh.registerUrl("dui.mobile.tests.robot.Flippable", require.toUrl("./Flippable.html"),999999);
+			doh.registerUrl("dui.mobile.tests.robot.Scrollable", require.toUrl("./ScrollableView.html"),999999);
+			doh.registerUrl("dui.mobile.tests.robot.Scrollable", require.toUrl("./ScrollableView2.html"),999999);
+			doh.registerUrl("dui.mobile.tests.robot.Scrollable", require.toUrl("./ScrollableView3.html"),999999);
 		}
 	}
-}catch(e){
-	doh.debug(e);
-}
+});
+
