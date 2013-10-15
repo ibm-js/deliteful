9f8cb3bad3ce054be2e397796fec3c3f550b0ac0
Christophe Jolif <cjolif@gmail.com>
2013-10-15 11:29:27 +0200

    improving test

--- tests/mixins/_Invalidating.js
+++ tests/mixins/_Invalidating.js
@@ -3,9 +3,9 @@ define(["doh/runner", "../../register", "../../mixins/_Invalidating", "../../_Wi
 	doh.register("mixins._Invalidating", [
 		{
 			timeout: 2000,
-			name: "Simple",
+			name: "PostCreation",
 			runTest: function(t){
-				var C = register("test-invalidating", [HTMLElement, _WidgetBase, _Invalidating], {
+				var C = register("test-invalidating-post", [HTMLElement, _WidgetBase, _Invalidating], {
 					preCreate: function(){
 						this.addInvalidatingProperties("a");
 						this.addInvalidatingProperties("b");
@@ -33,6 +33,37 @@ define(["doh/runner", "../../register", "../../mixins/_Invalidating", "../../_Wi
 				}), 1000);
 				return d;
 			}
-		}
+		},
+		{
+			timeout: 2000,
+			name: "InCreation",
+			runTest: function(t){
+				var C = register("test-invalidating-in", [HTMLElement, _WidgetBase, _Invalidating], {
+					preCreate: function(){
+						this.addInvalidatingProperties("a", "b");
+					},
+					a: null,
+					b: null,
+					refreshRendering: function(){
+						t.is({"b": true}, o.invalidatedProperties);
+					}
+				});
+				var d = new doh.Deferred();
+				var o = new C({b: "foo"});
+				var afterProps, beforeProps;
+				o.on("refresh-complete", function(e){
+					afterProps = o.invalidatedProperties;
+					beforeProps = e.invalidatedProperties;
+				});
+				o.startup();
+				t.is(["a", "b"], o._invalidatingProperties);
+				// we need to check before the timeout that refresh-complete was called
+				setTimeout(d.getTestCallback(function(){
+					t.is({}, afterProps);
+					t.is({"b": true}, beforeProps);
+				}), 1000);
+				return d;
+			}
+		},
 	]);
 });
