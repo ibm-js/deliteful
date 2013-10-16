c802633c8872e37226d752464f8424d49e460f16
Christophe Jolif <cjolif@gmail.com>
2013-10-16 12:59:02 +0200

    add a refresh properties step to the lifecycle, fix according to jshint, add more tests

--- mixins/_Invalidating.js
+++ mixins/_Invalidating.js
@@ -1,74 +1,147 @@
-define(["dcl/dcl", "dojo/_base/lang", "../_WidgetBase"],
-	function(dcl, lang, _WidgetBase){
-		
+define(["dcl/dcl", "dojo/_base/lang", "../_WidgetBase"], function (dcl, lang, _WidgetBase) {
+
 	return dcl(_WidgetBase, {
 		// summary:
 		//		Mixin for classes (usually widgets) that watch invalidated properties and delay the rendering
 		//		after these properties modifications to the next execution frame. The receiving class must extend
 		//		dojo/Stateful and dojo/Evented or dui/_WidgetBase.
-		
-		// _invalidatingProperties: String[]
-		//		The list of properties to watch for to trigger invalidation. This list must be initialized
-		//		by the time buildRendering() completes, usually in preCreate(). Default value is null.
+
+		// _invalidatingProperties: {}
+		//		A hash of properties to watch for to trigger properties invalidation and/or rendering
+		//		invalidation.
+		//		This list must be initialized by the time buildRendering() completes, usually in preCreate()
+		//		using addInvalidatingProperties. Default value is null.
 		_invalidatingProperties: null,
 		// invalidatedProperties: Object
-		//		A hash of invalidated properties.
-		invalidatedProperties: {},
+		//		A hash of invalidated properties either to refresh them or refresh the rendering
+		invalidatedProperties: null,
+		// invalidProperties: Boolean
+		//		Whether at least one property is invalid or not. This is a readonly information, one must call
+		//		invalidateProperties to modify this flag.
+		invalidProperties: false,
 		// invalidRenderering: Boolean
-		//		Whether the rendering is invalid or not. This is a readonly information, one must call 
-		//		invalidateRendering to modify this flag. 
+		//		Whether the rendering is invalid or not. This is a readonly information, one must call
+		//		invalidateRendering to modify this flag.
 		invalidRendering: false,
 
 		// Listen for any changes to properties after the widget has been rendered,
 		// including when declarative properties (ex: iconClass=xyz) are applied at
 		// end of _WidgetBase.createdCallback().
-		buildRendering: dcl.after(function(){
+		buildRendering: dcl.after(function () {
 			// tags:
 			//		protected
-			if(this._invalidatingProperties){
-				var props = this._invalidatingProperties;
-				for(var i = 0; i < props.length; i++){
-					this.watch(props[i], lang.hitch(this, "invalidateRendering"));
+			if (this._invalidatingProperties) {
+				var props = Object.keys(this._invalidatingProperties);
+				for (var i = 0; i < props.length; i++) {
+					this.watch(props[i], lang.hitch(this, this._invalidatingProperties[props[i]]));
 				}
 			}
+			this.invalidatedProperties = {};
 		}),
 
-		addInvalidatingProperties: function(){
+		addInvalidatingProperties: function () {
 			// summary:
-			//		Add the properties listed as parameters to the watched properties to trigger invalidation. This method
-			// 		must be called during the startup lifecycle, before buildRendering() completes.
+			//		Add the properties listed as parameters to the watched properties to trigger invalidation.
+			// 		This method must be called during the startup lifecycle, before buildRendering() completes.
 			//		It is typically used by subclasses of a _Invalidating class to
 			// 		add more properties	to watch for.
 			// tags:
 			//		protected
-			this._invalidatingProperties = this._invalidatingProperties?
-				this._invalidatingProperties.concat(Array.prototype.slice.call(arguments)):Array.prototype.slice.call(arguments);
+			if (this._invalidatingProperties == null) {
+				this._invalidatingProperties = {};
+			}
+			for (var i = 0; i < arguments.length; i++) {
+				if (typeof arguments[i] === "string") {
+					// we just want the rendering to be refreshed
+					this._invalidatingProperties[arguments[i]] = "invalidateRendering";
+				} else {
+					// we just merge object keys/values into our invalidate list
+					var props = Object.keys(arguments[i]);
+					for (var j = 0; j < props.length; j++) {
+						this._invalidatingProperties[props[j]] = arguments[i][props[j]];
+					}
+				}
+			}
 		},
-		invalidateRendering: function(name){
+		invalidateProperty: function (name) {
 			// summary:
-			//		Invalidating the rendering for the next execution frame.
+			//		Invalidating the property for the next execution frame.
+			// name: String?
+			//		The name of the property to invalidate. If absent then re-validation is asked without a
+			//		particular property being invalidated.
 			// tags:
 			//		protected
-			this.invalidatedProperties[name] = true;
-			if(!this.invalidRendering){
+			if (name) {
+				this.invalidatedProperties[name] = true;
+			}
+			if (!this.invalidProperties) {
+				this.invalidProperties = true;
+				setTimeout(lang.hitch(this, "validateProperties"), 0);
+			}
+		},
+		invalidateRendering: function (name) {
+			// summary:
+			//		Invalidating the rendering for the next execution frame.
+			// name: String?
+			//		The name of the property to invalidate. If absent then re-validation is asked without a
+			//		particular property being invalidated.
+			// tags:
+			//		protected
+			if (name) {
+				this.invalidatedProperties[name] = true;
+			}
+			if (!this.invalidRendering) {
 				this.invalidRendering = true;
 				this.defer("validateRendering");
 			}
 		},
-		validateRendering: function(){
+		validateProperties: function () {
 			// summary:
-			//		Immediately validate the rendering if it has been invalidated. You generally do not call that method yourself.
+			//		Immediately validate the properties if they have been invalidated.
+			//		You generally do not call that method yourself.
 			// tags:
 			//		protected
-			if(this.invalidRendering){
-				this.refreshRendering();
-				var props = this.invalidatedProperties;
-				this.invalidatedProperties = {};
-				this.invalidRendering = false;
-				this.emit("refresh-complete", { invalidatedProperties: props, bubbles: true, cancelable: false });
+			if (this.invalidProperties) {
+				var props = lang.clone(this.invalidatedProperties);
+				this.invalidProperties = false;
+				this.refreshProperties();
+				this.emit("refresh-properties-complete",
+					{ invalidatedProperties: props, bubbles: true, cancelable: false });
+				// if there are properties still marked invalid pursue further with rendering refresh
+				this.invalidateRendering();
 			}
 		},
-		refreshRendering: function(){
+		validateRendering: function () {
+			// summary:
+			//		Immediately validate the rendering if it has been invalidated.
+			//		You generally do not call that method yourself.
+			// tags:
+			//		protected
+			if (this.invalidRendering) {
+				var props = lang.clone(this.invalidatedProperties);
+				this.invalidRendering = false;
+				this.refreshRendering();
+				this.invalidatedProperties = {};
+				this.emit("refresh-rendering-complete",
+					{ invalidatedProperties: props, bubbles: true, cancelable: false });
+			}
+		},
+		validate: function () {
+			// summary:
+			//		Immediately validate the properties & the rendering if it has been invalidated.
+			//		You generally do not call that method yourself.
+			// tags:
+			//		protected
+			this.validateProperties();
+			this.validateRendering();
+		},
+		refreshProperties: function () {
+			// summary:
+			//		Actually refresh the properties. Implementation should implement that method.
+			// tags:
+			//		protected
+		},
+		refreshRendering: function () {
 			// summary:
 			//		Actually refresh the rendering. Implementation should implement that method.
 			// tags:
--- tests/mixins/_Invalidating.js
+++ tests/mixins/_Invalidating.js
@@ -1,35 +1,41 @@
 define(["doh/runner", "../../register", "../../mixins/_Invalidating", "../../_WidgetBase"],
-	function(doh, register, _Invalidating, _WidgetBase){
+	function (doh, register, _Invalidating, _WidgetBase) {
 	doh.register("mixins._Invalidating", [
 		{
 			timeout: 2000,
 			name: "PostCreation",
-			runTest: function(t){
+			runTest: function (t) {
 				var C = register("test-invalidating-post", [HTMLElement, _WidgetBase, _Invalidating], {
-					preCreate: function(){
+					preCreate: function () {
 						this.addInvalidatingProperties("a");
 						this.addInvalidatingProperties("b");
 					},
 					a: null,
 					b: null,
-					refreshRendering: function(){
-						t.is({"b": true}, o.invalidatedProperties);
+					refreshProperties: function () {
+						t.is(true, false, "refreshProperties should not be called");
+					},
+					refreshRendering: function () {
+						t.is({"b": true}, this.invalidatedProperties);
 					}
 				});
 				var d = new doh.Deferred();
 				var o = new C();
-				var afterProps, beforeProps;
-				o.on("refresh-complete", function(e){
-					afterProps = o.invalidatedProperties;
-					beforeProps = e.invalidatedProperties;
+				var afterPropsR, beforePropsR;
+				o.on("refresh-properties-complete", function () {
+					t.is(true, false, "refreshProperties should not be called");
+				});
+				o.on("refresh-rendering-complete", function (e) {
+					afterPropsR = o.invalidatedProperties;
+					beforePropsR = e.invalidatedProperties;
 				});
 				o.startup();
-				t.is(["a", "b"], o._invalidatingProperties);
+				t.is({ "a": "invalidateRendering", "b": "invalidateRendering" }, o._invalidatingProperties);
 				o.b = "foo";
 				// we need to check before the timeout that refresh-complete was called
-				setTimeout(d.getTestCallback(function(){
-					t.is({}, afterProps);
-					t.is({"b": true}, beforeProps);
+				setTimeout(d.getTestCallback(function () {
+					t.is({}, afterPropsR);
+					t.is({"b": true}, beforePropsR);
 				}), 1000);
 				return d;
 			}
@@ -37,33 +43,165 @@ define(["doh/runner", "../../register", "../../mixins/_Invalidating", "../../_Wi
 		{
 			timeout: 2000,
 			name: "InCreation",
-			runTest: function(t){
+			runTest: function (t) {
 				var C = register("test-invalidating-in", [HTMLElement, _WidgetBase, _Invalidating], {
-					preCreate: function(){
+					preCreate: function () {
 						this.addInvalidatingProperties("a", "b");
 					},
 					a: null,
 					b: null,
-					refreshRendering: function(){
-						t.is({"b": true}, o.invalidatedProperties);
+					refreshProperties: function () {
+						t.is(true, false, "refreshProperties should not be called");
+					},
+					refreshRendering: function () {
+						t.is({"b": true}, this.invalidatedProperties);
 					}
 				});
 				var d = new doh.Deferred();
 				var o = new C({b: "foo"});
-				var afterProps, beforeProps;
-				o.on("refresh-complete", function(e){
-					afterProps = o.invalidatedProperties;
-					beforeProps = e.invalidatedProperties;
+				var afterPropsR, beforePropsR;
+				o.on("refresh-properties-complete", function () {
+					t.is(true, false, "refreshProperties should not be called");
+				});
+				o.on("refresh-rendering-complete", function (e) {
+					afterPropsR = o.invalidatedProperties;
+					beforePropsR = e.invalidatedProperties;
 				});
 				o.startup();
-				t.is(["a", "b"], o._invalidatingProperties);
+				t.is({ "a": "invalidateRendering", "b": "invalidateRendering" }, o._invalidatingProperties);
 				// we need to check before the timeout that refresh-complete was called
-				setTimeout(d.getTestCallback(function(){
-					t.is({}, afterProps);
-					t.is({"b": true}, beforeProps);
+				setTimeout(d.getTestCallback(function () {
+					t.is({}, afterPropsR);
+					t.is({"b": true}, beforePropsR);
 				}), 1000);
 				return d;
 			}
 		},
+		{
+			timeout: 2000,
+			name: "OnlyRefreshProperty",
+			runTest: function (t) {
+				var C = register("test-invalidating-only-refresh-props", [HTMLElement, _WidgetBase, _Invalidating], {
+					preCreate: function () {
+						this.addInvalidatingProperties({"a": "invalidateProperty"});
+						this.addInvalidatingProperties({"b": "invalidateProperty"});
+					},
+					a: null,
+					b: null,
+					refreshProperties: function () {
+						t.is({"a": true, "b": true}, this.invalidatedProperties);
+						// only a should lead to a refreshRendering
+						delete this.invalidatedProperties.b;
+
+					},
+					refreshRendering: function () {
+						t.is({"a": true}, o.invalidatedProperties);
+					}
+				});
+				var d = new doh.Deferred();
+				var o = new C();
+				var afterPropsR, beforePropsR, afterPropsP, beforePropsP;
+				o.on("refresh-properties-complete", function (e) {
+					afterPropsP = o.invalidatedProperties;
+					beforePropsP = e.invalidatedProperties;
+				});
+				o.on("refresh-rendering-complete", function (e) {
+					afterPropsR = o.invalidatedProperties;
+					beforePropsR = e.invalidatedProperties;
+				});
+				o.startup();
+				t.is({ "a": "invalidateProperty", "b": "invalidateProperty" }, o._invalidatingProperties);
+				o.b = "foo";
+				o.a = "bar";
+				// we need to check before the timeout that refresh-complete was called
+				setTimeout(d.getTestCallback(function () {
+					t.is({"a" : true}, afterPropsP);
+					t.is({"a" : true, "b": true}, beforePropsP);
+					t.is({}, afterPropsR);
+					t.is({"a": true}, beforePropsR);
+				}), 1000);
+				return d;
+			}
+		},
+		{
+			timeout: 2000,
+			name: "Manual",
+			runTest: function (t) {
+				var C = register("test-invalidating-manual", [HTMLElement, _WidgetBase, _Invalidating], {
+					a: null,
+					b: null,
+					refreshProperties: function () {
+						t.is({"b": true}, this.invalidatedProperties);
+					},
+					refreshRendering: function () {
+						t.is({"b": true}, this.invalidatedProperties);
+					}
+				});
+				var d = new doh.Deferred();
+				var o = new C();
+				var afterPropsR, beforePropsR, afterPropsP, beforePropsP;
+				o.on("refresh-properties-complete", function (e) {
+					afterPropsP = o.invalidatedProperties;
+					beforePropsP = e.invalidatedProperties;
+				});
+				o.on("refresh-rendering-complete", function (e) {
+					afterPropsR = o.invalidatedProperties;
+					beforePropsR = e.invalidatedProperties;
+				});
+				o.startup();
+				t.is(null, o._invalidatingProperties);
+				o.b = "foo";
+				o.invalidateProperty("b");
+				// we need to check before the timeout that refresh-complete was called
+				setTimeout(d.getTestCallback(function () {
+					t.is({"b": true}, afterPropsP);
+					t.is({"b": true}, beforePropsP);
+					t.is({}, afterPropsR);
+					t.is({"b": true}, beforePropsR);
+				}), 1000);
+				return d;
+			}
+		},
+		{
+			timeout: 2000,
+			name: "PropertyAndRendering",
+			runTest: function (t) {
+				var C = register("test-invalidating-prop-rendering", [HTMLElement, _WidgetBase, _Invalidating], {
+					preCreate: function () {
+						this.addInvalidatingProperties({"a": "invalidateProperty", "b": "invalidateProperty"});
+					},
+					a: null,
+					b: null,
+					refreshProperties: function () {
+						t.is({"b": true}, this.invalidatedProperties);
+					},
+					refreshRendering: function () {
+						t.is({"b": true}, this.invalidatedProperties);
+					}
+				});
+				var d = new doh.Deferred();
+				var o = new C();
+				var afterPropsR, beforePropsR, afterPropsP, beforePropsP;
+				o.on("refresh-properties-complete", function (e) {
+					afterPropsP = o.invalidatedProperties;
+					beforePropsP = e.invalidatedProperties;
+				});
+				o.on("refresh-rendering-complete", function (e) {
+					afterPropsR = o.invalidatedProperties;
+					beforePropsR = e.invalidatedProperties;
+				});
+				o.startup();
+				t.is({ "a": "invalidateProperty", "b": "invalidateProperty" }, o._invalidatingProperties);
+				o.b = "foo";
+				// we need to check before the timeout that refresh-complete was called
+				setTimeout(d.getTestCallback(function () {
+					t.is({"b": true}, afterPropsP);
+					t.is({"b": true}, beforePropsP);
+					t.is({}, afterPropsR);
+					t.is({"b": true}, beforePropsR);
+				}), 1000);
+				return d;
+			}
+		}
 	]);
 });
