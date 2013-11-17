a2f20737462b280bc0a382ac39736d8758acb222
doughays-dojo <doughays@us.ibm.com>
2013-11-16 17:05:29 -0800

    Refactor Slider to use refreshRendering instead of _setXXXAttr.

--- Slider.js
+++ Slider.js
@@ -21,17 +21,13 @@ define([
 		// summary:
 		//		A non-templated Slider widget similar to the HTML5 INPUT type=range.
 
-		// value: String
-		//		The current slider value, either String(Number) or String(Number,Number).
-		//value: "",
-
 		// min: [const] Number
 		//		The first value the slider can be set to.
-		min: 0,
+		min: NaN,
 
 		// max: [const] Number
 		//		The last value the slider can be set to.
-		max: 100,
+		max: NaN,
 
 		// step: [const] Number
 		//		The delta from 1 value to another.
@@ -48,28 +44,106 @@ define([
 		flip: false,
 
 		// orientation: [const] String
-		//		The slider direction.
-		//
-		//		- "H": horizontal
+		//		The slider direction for ascending values.
+		//		- "H": horizontal (dependent on default left-to-right or right-to-left direction)
 		//		- "V": vertical
 		orientation: "H",
 
-		// isRange: [const] Boolean
-		//		True if there's both a visible min and max slider handle
-		isRange: false,
+		preCreate: function () {
+			this._orientationAttrs = {
+				"H": { start: "x", size: "w", pageStart: "pageX", clientStart: "clientX",
+						progressBarStart: "left", progressBarSize: "width" },
+				"V": { start: "y", size: "h", pageStart: "pageY", clientStart: "clientY", progressBarStart: "top",
+						progressBarSize: "height" }
+			};
 
-		tabStops: "handle",
+			this.addInvalidatingProperties(
+				{ value: "invalidateProperty" },
+				{ min: "invalidateProperty" },
+				{ max: "invalidateProperty" },
+				"name",
+				"flip",
+				"step",
+				"orientation"
+			);
+		},
+
+		refreshProperties: function (props) {
+			if (props.value && !this.handleMin && String(this.value).indexOf(",") > 0) {
+				this.handleMin = domConstruct.create("div", { role: "slider" }, this.progressBar, "first");
+				this.tabStops = "handleMin,focusNode";
+			}
+			if (props.value || props.min || props.max) {
+				// force the value in bounds
+				var values = String(this.value).split(/,/g);
+				var	minValue = Math.max(Math.min(values[0], values[values.length-1], this.max), this.min),
+					maxValue = Math.min(Math.max(values[0], values[values.length-1], this.min), this.max);
+				// correct value in case the values were outside min/max
+				var correctedValue = values.length === 1 ? String(maxValue) : (minValue + "," + maxValue);
+				if (correctedValue !== this.value) {
+					this.value = correctedValue;
+					this.validateProperties();
+				}
+			}
+			if (props.value) {
+				this.valueNode.value = String(this.value);
+			}
+		},
+
+		refreshRendering: register.before(function (props) {
+			var toCSS = function (baseClass, modifier) {
+				return baseClass.split(/ /g).map(function (c) {
+					return c + modifier;
+				}).join(" ");
+			};
+
+			if (props.orientation || props.flip) {
+				// add V or H suffix to baseClass for styling purposes
+				var	horizontal = this.orientation !== "V",
+					ltr = horizontal ? this.isLeftToRight() : false,
+					flip = !!this.flip;
+				// _reversed is complicated since you can have flipped right-to-left and vertical is upside down by default
+				this._reversed = !((horizontal && ((ltr && !flip) || (!ltr && flip))) || (!horizontal && flip));
+				this._attrs = this._orientationAttrs[this.orientation];
+				var baseClass = toCSS(this.className, this.orientation);
+				domClass.add(this, baseClass);
+				baseClass = this.baseClass + " " + baseClass;
+				domClass.add(this, toCSS(baseClass, this._reversed ? "HtL" : "LtH"));
+				domClass.add(this.containerNode, toCSS(baseClass, "Bar") + " " + toCSS(baseClass, "RemainingBar"));
+				domClass.add(this.progressBar, toCSS(baseClass, "Bar") + " " + toCSS(baseClass, "ProgressBar"));
+				domClass.add(this.focusNode, toCSS(baseClass, "Handle") + " " + toCSS(baseClass, "HandleMax"));
+				if (this.handleMin) {
+					domClass.add(this.handleMin, toCSS(baseClass, "Handle") + " " + toCSS(baseClass, "HandleMin"));
+				}
+				// pass widget attributes to children
+				var self = this;
+				this.getChildren().forEach(function (obj) {
+					obj.orientation = self.orientation;
+					obj.reverse = self._reversed;
+				});
+			}
+			if (props.name) {
+				this.valueNode.name = this.name;
+			}
+			if (props.max) {
+				this.focusNode.setAttribute("aria-valuemax", this.max);
+			}
+			if (props.min) {
+				(this.handleMin || this.focusNode).setAttribute("aria-valuemin", this.min);
+			}
+			if (props.value || props.min || props.max) {
+				this._positionHandles();
+			}
+		}),
 
 		buildRendering: function () {
+
 			// look for child INPUT node under root node
 			this.valueNode = query("> INPUT", this)[0];
 			if (!this.valueNode) {
-				this.valueNode = domConstruct.create("input", { "type": "text", readOnly: true }, this, "last");
+				this.valueNode = domConstruct.create("input", { "type": "text", readOnly: true, value: this.value }, this, "last");
 			}
 
-			if (!isNaN(parseFloat(this.valueNode.value))) { // browser back button or value coded on INPUT
-				this.value = this.valueNode.value;
-			}
 			this.containerNode = domConstruct.create("div", {}, this, "last");
 			var n = this.firstChild;
 			while (n) {
@@ -81,9 +155,7 @@ define([
 				n = next;
 			}
 			this.progressBar = domConstruct.create("div", {}, this.containerNode, "last");
-			this.handle = domConstruct.create("div", { tabIndex: this.tabIndex }, this.progressBar, "last");
-
-			this.focusNode = this.handle;
+			this.focusNode = domConstruct.create("div", { role: "slider" }, this.progressBar, "last");
 
 			// prevent browser scrolling on IE10 (evt.preventDefault() is not enough)
 			if (typeof this.style.msTouchAction !== "undefined") {
@@ -91,66 +163,41 @@ define([
 			}
 		},
 
-		_refreshState: function (/*Boolean?*/ priorityChange) {
-			this.valueNode.value = String(this.value);
-			var values = String(this.value).split(",");
+		_positionHandles: function () {
+			var values = String(this.value).split(/,/g);
 			if (values.length === 1) {
 				values = [this.min, values[0]];
-			}
-			if (this.isRange) {
+			} else {
 				this.handleMin.setAttribute("aria-valuenow", values[0]);
+				this.handleMin.setAttribute("aria-valuemax", values[1]);
 			}
-			this.handle.setAttribute("aria-valuenow", values[1]);
+			this.focusNode.setAttribute("aria-valuenow", values[1]);
+			this.focusNode.setAttribute("aria-valuemin", values[0]);
 			var	toPercent = (values[1] - this.min) * 100 / (this.max - this.min),
 				toPercentMin = (values[0] - this.min) * 100 / (this.max - this.min),
 				s = {};
-			// now perform visual slide
-			domClass.toggle(this.progressBar, "duiSliderTransition", priorityChange);
 			s[this._attrs.progressBarSize] = (toPercent - toPercentMin) + "%";
 			s[this._attrs.progressBarStart] = (this._reversed ? (100 - toPercent) : toPercentMin) + "%";
 			domStyle.set(this.progressBar, s);
 		},
-
-		_setValueAttr: function (/*Number or String(Number) or String(Number,Number)*/ value) {
-			// summary:
-			//		Hook such that set("value", value) works.
-			// tags:
-			//		private
-			this._handleOnChange(value, null);
-		},
-
-		_handleOnChange: register.before(function (
-			/*Number or String(Number) or String(Number,Number)*/ value,
-			/*Boolean?*/ priorityChange) {
-			if (this._started) {
-				// force the value in bounds
-				var values = String(value).split(",");
-				if (values.length === 1) {
-					values = [this.min, values[0]];
-				}
-				var	minValue = Math.max(Math.min(Math.min(values[0], values[1]), this.max), this.min),
-					maxValue = Math.max(Math.min(Math.max(values[0], values[1]), this.max), this.min);
-				// correct value in case the values were outside min/max
-				value = this.isRange ? (minValue + "," + maxValue) : maxValue;
-			}
-			this._set("value", value);
-			if (this._started) {
-				this._refreshState(priorityChange);
-			} else {
-				this.valueNode.setAttribute("value", this.value); // set the reset value
-			}
-		}),
 
 		postCreate: function () {
 			var	beginDrag = lang.hitch(this,
 				function (e) {
 					e.preventDefault();
 					var	setValue = lang.hitch(this, function (priorityChange) {
-							var values = String(this.value).split(",");
+							var values = String(this.value).split(/,/g);
 							value -= offsetValue;
-							this._handleOnChange(this.isRange
-								? (isMax ? (values[0] + "," + value)
-								: (value + "," + values[1])) : value, priorityChange);
+							// now perform visual slide
+							domClass.toggle(this.progressBar, "duiSliderTransition", !!priorityChange);
+							this.value = 
+								values.length === 1
+									? String(value)
+									: (isMax
+										? (values[0] + "," + value)
+										: (value + "," + values[1]));
+							this.validateProperties();
+							this._handleOnChange(this.value, priorityChange);
 						}),
 						getEventData = lang.hitch(this, function (e) {
 							point = isMouse
@@ -198,14 +245,14 @@ define([
 					var maxPixels = box[this._attrs.size] * nodeZoom * bodyZoom;
 					var offsetValue = 0;
 					getEventData(e);
-					var values = String(this.value).split(",");
-					var isMax = !this.isRange || (Math.abs(value - values[0]) > Math.abs(value - values[1]));
-					if (e.target !== this.handle && e.target !== this.handleMin) {
+					var values = String(this.value).split(/,/g);
+					var isMax = (values.length === 1) || (Math.abs(value - values[0]) > Math.abs(value - values[1]));
+					if (e.target !== this.focusNode && e.target !== this.handleMin) {
 						setValue(true);
 					} else {
-						offsetValue = value - ((isMax && this.isRange) ? values[1] : values[0]);
+						offsetValue = value - ((isMax && this.handleMin) ? values[1] : values[0]);
 					}
-					this[isMax ? "handle" : "handleMin"].focus();
+					this[isMax ? "focusNode" : "handleMin"].focus();
 					if (actionHandles) {
 						actionHandles.forEach(function (h) { h.remove(); });
 					}
@@ -245,15 +292,17 @@ define([
 								return;
 						}
 						e.preventDefault();
-						this._handleOnChange(values.join(","), false);
+						this.value = values.join(",");
+						this.validateProperties();
+						this._handleOnChange(this.value, false);
 					}
 
 					if (this.disabled || this.readOnly || e.altKey || e.ctrlKey || e.metaKey) { return; }
 					var	handleIdx = 0,
 						maxValue = this.max,
-						values = String(this.value).split(",");
-					if (e.target === this.handle) {
-						handleIdx = this.isRange ? 1 : 0;
+						values = String(this.value).split(/,/g);
+					if (e.target === this.focusNode) {
+						handleIdx = values.length - 1;
 					} else if (e.target === this.handleMin) {
 						maxValue = values[1];
 					} else {
@@ -269,40 +318,20 @@ define([
 
 				point, pixelValue, value,
 				node = this;
+			if (!isNaN(parseFloat(this.valueNode.value))) { // browser back button or value coded on INPUT
+				this.value = this.valueNode.value;
+			}
 			this.own(
 				on(this, touch.press, beginDrag),
 				on(this, "keydown", keyDown), // for desktop a11y
-				on(this.handle, "keyup", keyUp) // fire onChange on desktop
+				on(this.focusNode, "keyup", keyUp) // fire onChange on desktop
 			);
+			this.invalidateProperty("orientation"); // apply appropriate CSS class names in case the default is used
+			this.invalidateProperty("tabIndex"); // apply default tabIndex in case the default is used
 		},
 
-		_setIsRangeAttr: function (/*Boolean*/ value) {
-			this._set("isRange", value);
-			if (value) {
-				this.handleMin = domConstruct.create("div", {
-					disabled: this.disabled,
-					tabIndex: this.tabIndex
-				}, this.progressBar, "first");
-				this.tabStops = "handleMin,handle";
-			}
-		},
-
-		startup: register.before(function () {
-			var self = this;
-
-			var toCSS = function (baseClass, modifier) {
-				return baseClass.split(" ").map(function (c) {
-					return c + modifier;
-				}).join(" ");
-			};
-
-			// add V or H suffix to baseClass for styling purposes
-			var	horizontal = this.orientation !== "V",
-				ltr = horizontal ? this.isLeftToRight() : false,
-				flip = !!this.flip;
-			if (this.name) {
-				this.valueNode.name = this.name;
-			}
+		startup: function () {
+			this.valueNode.setAttribute("value", this.value); // set the reset value once
 			// if the form is reset, then notify the widget to reposition the handles
 			if (this.valueNode.form) {
 				this.own(on(this.valueNode.form, "reset", lang.hitch(this, "defer",
@@ -313,32 +342,6 @@ define([
 					}, 0
 				)));
 			}
-			// _reversed is complicated since you can have flipped right-to-left and vertical is upside down by default
-			this._reversed = !((horizontal && ((ltr && !flip) || (!ltr && flip))) || (!horizontal && flip));
-			this._attrs = horizontal
-				? { start: "x", size: "w", pageStart: "pageX", clientStart: "clientX",
-					progressBarStart: "left", progressBarSize: "width" }
-				: { start: "y", size: "h", pageStart: "pageY", clientStart: "clientY", progressBarStart: "top",
-					progressBarSize: "height" };
-			var baseClass = toCSS(this.className, this.orientation);
-			domClass.add(this, baseClass);
-			baseClass = this.baseClass + " " + baseClass;
-			domClass.add(this, toCSS(baseClass, this._reversed ? "HtL" : "LtH"));
-			domClass.add(this.containerNode, toCSS(baseClass, "Bar") + " " + toCSS(baseClass, "RemainingBar"));
-			domClass.add(this.progressBar, toCSS(baseClass, "Bar") + " " + toCSS(baseClass, "ProgressBar"));
-			domClass.add(this.handle, toCSS(baseClass, "Handle") + " " + toCSS(baseClass, "HandleMax"));
-			if (this.handleMin) {
-				domClass.add(this.handleMin, toCSS(baseClass, "Handle") + " " + toCSS(baseClass, "HandleMin"));
-			}
-
-			// pass widget attributes to children
-			this.getChildren().forEach(function (obj) {
-				if (!obj._started && !obj._destroyed && lang.isFunction(obj._parentInit)) {
-					obj._parentInit(self._reversed, self.orientation);
-				}
-			});
-
-			this._refreshState(null);
-		})
+		}
 	});
 });
