b93083225db2a005d026c7e8c5e79b97297c2688
Adrian Vasiliu <vasiliu@fr.ibm.com>
2014-01-13 18:11:54 +0100

    deliteful/ScrollableContainer

---
+++ ScrollableContainer.js
@@ -0,0 +1,29 @@
+define([
+	"delite/register",
+	"delite/Widget",
+	"delite/Container",
+	"delite/Scrollable"
+], function (register, Widget, Container, Scrollable) {
+
+	// module:
+	//		deliteful/ScrollableContainer
+	
+	return register("d-scrollable-container", [HTMLElement, Widget, Container, Scrollable], {
+		// summary:
+		//		A container widget with scrolling capabilities.
+		// description:
+		//		A container widget which can scroll its content 
+		//		horizontally and/or vertically. Its scrolling capabilities 
+		//		and API are provided by the mixin delite/Scrollable.
+		// example:
+		//	|	<d-scrollable-container>
+		//	|		<div>...</div>
+		//	|		<div>...</div>
+		//	|		<div>...</div>
+		//	|	</d-scrollable-container>
+
+		// baseClass: String
+		//		The name of the CSS class of this widget.
+		baseClass: "d-scrollable-container"
+	});
+});
