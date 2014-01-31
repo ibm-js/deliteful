36ad53bc2feaed5cb93573bc101adf0f57415ed8
Adrian Vasiliu <vasiliu@fr.ibm.com>
2014-01-31 14:09:41 +0100

    Added deliteful/ScrollableContainer

--- tests/unit.js
+++ tests/unit.js
@@ -5,5 +5,6 @@ define(["./unit/LinearLayout1",
 		"./unit/LinearLayout4",
 		"./unit/LinearLayout5",
 		"./unit/SidePane.js",
-		"./unit/ViewStack.js"
+		"./unit/ViewStack.js",
+		"./unit/ScrollableContainer"
 ]);
