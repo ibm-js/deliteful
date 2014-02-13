6994469a1b2ea9bc7ce9f6407720f3d42dd9954d
Sebastien Brunot <sbrunot@gmail.com>
2014-02-13 10:44:26 +0100

    upgrade intern to version 1.4.0 and leverage the new lcovhtml coverage report.

--- tests/local.js
+++ tests/local.js
@@ -4,7 +4,9 @@
 define({
 	// Browsers to run tests against
 	environments: [
-		{ browserName: "chrome" }
+		{ browserName: "chrome" },
+		{ browserName: "firefox" },
+		{ browserName: "iexplorer" }
 	],
 
 	// Whether or not to start Sauce Connect before running tests
@@ -15,6 +17,8 @@ define({
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
 	functionalSuites: ["deliteful/tests/functional/all"],
+
+	reporters: ["runner", "lcovhtml"],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(requirejs|dcl|dojo|dpointer|delite\/|deliteful\/tests|.*themes|.*transitions)/
