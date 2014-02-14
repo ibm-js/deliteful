91e3a6ef8986b10162c449580091dfcd8b4bca58
Sebastien Brunot <sbrunot@gmail.com>
2014-02-14 15:50:43 +0100

    update mobile tests configuration

--- tests/sauce.js
+++ tests/sauce.js
@@ -36,16 +36,17 @@ define({
 		{ browserName: "safari", version: "6", platform: [ "OS X 10.8" ] },
 
 		// Mobile
+		{ browserName: "android", platform: "Linux", version: "4.1" },
 		{ browserName: "android", platform: "Linux", "device-type": "tablet", version: "4.1" },
 		{ browserName: "android", platform: "Linux", version: "4.1" },
 		{ browserName: "android", platform: "Linux", "device-type": "tablet", version: "4.0" },
 		{ browserName: "android", platform: "Linux", version: "4.0" },
-		{ browserName: "iphone", platform: "OS X 10.9", version: "7" },
-		{ browserName: "ipad", platform: "OS X 10.9", version: "7" },
-		{ browserName: "iphone", platform: "OS X 10.8", version: "6.1" },
-		{ browserName: "ipad", platform: "OS X 10.8", version: "6.1" },
-		{ browserName: "iphone", platform: "OS X 10.8", version: "6.0" },
-		{ browserName: "ipad", platform: "OS X 10.8", version: "6.0" }
+		{ browserName: "iphone", platform: "OS X 10.9", version: "7", "selenium-version": "" },
+		{ browserName: "ipad", platform: "OS X 10.9", version: "7", "selenium-version": "" },
+		{ browserName: "iphone", platform: "OS X 10.8", version: "6.1", "selenium-version": "" },
+		{ browserName: "ipad", platform: "OS X 10.8", version: "6.1", "selenium-version": "" },
+		{ browserName: "iphone", platform: "OS X 10.8", version: "6.0", "selenium-version": "" },
+		{ browserName: "ipad", platform: "OS X 10.8", version: "6.0", "selenium-version": "" },
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
