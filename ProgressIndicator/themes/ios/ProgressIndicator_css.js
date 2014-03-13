a34c0b948a22f08db09238541cb805a733d56f45
Sebastien Pereira <spereira@javalive.net>
2014-03-13 15:33:38 +0100

    Add ProgressIndicator widget

---
+++ ProgressIndicator/themes/ios/ProgressIndicator_css.js
@@ -0,0 +1,33 @@
+define(function(){ return '\
+/*\
+ * -----------------------------------------------\
+ *  Theme     : ios\
+ *  Widget    : deliteful/ProgressIndicator\
+ *  baseClass : d-progress-indicator\
+ * -----------------------------------------------\
+ */\
+.d-progress-indicator {\
+  display: inline-block;\
+  visibility: hidden;\
+  vertical-align: middle;\
+  background-color: transparent;\
+  -webkit-touch-callout: none;\
+  -webkit-user-select: none;\
+  -khtml-user-select: none;\
+  -moz-user-select: none;\
+  -ms-user-select: none;\
+  user-select: none;\
+  width: 40px;\
+  height: 40px;\
+  font-size: 16px;\
+  font-weight: bold;\
+}\
+.d-progress-indicator-lines {\
+  stroke-width: 8%;\
+  stroke-linecap: round;\
+  stroke: #000000;\
+}\
+.d-progress-indicator text {\
+  fill: #000000;\
+}\
+'; } );
