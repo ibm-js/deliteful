8bae7adcda6daf4293793690cdc4ad7fd76c9832
Sebastien Brunot <sbrunot@gmail.com>
2014-04-11 16:09:40 +0200

    Add configuration and documentation to run tests locally on android browsers

---
+++ tests/intern.local.android.js
@@ -0,0 +1,23 @@
+// run grunt --help for help on how to run
+define([
+	"./intern"
+], function (intern) {
+
+	// TODO: update this variable to use the IP address of your host
+	host = "192.168.0.21";
+
+	intern.useSauceConnect = false;
+	intern.webdriver = {
+		host: "localhost",
+		port: 4444
+	};
+
+	intern.environments = [
+		{ browserName: "android", androidTarget: "19" }, // Android 4.4
+		{ browserName: "android", androidTarget: "18" } // Android 4.3
+	];
+
+	intern.proxyUrl= "http://" + host + ":9000/";
+
+	return intern;
+});
