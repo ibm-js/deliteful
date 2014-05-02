fdf564920f03842b9e4d528b2506cf4f387f5c7e
Patrick Ruzand <pruzand@gmail.com>
2014-05-02 08:58:43 +0200

    Fixes for running intern tests locally in browser. the fix is similar to delite one, except that baseUrl is not changed

---
+++ tests/intern.browser.js
@@ -0,0 +1,13 @@
+// Test file to run infrastructure tests from a browser
+// Run using http://localhost/delite/node_modules/intern/client.html?config=tests/intern-browser
+
+define([
+	"./intern"
+], function (intern) {
+
+	intern.loader = {
+		baseUrl: "../../.."
+	};
+
+	return intern;
+});
