b8ba570d893dc4c3d6fd77b74bcc4a05b04fadad
Adrian Vasiliu <vasiliu@fr.ibm.com>
2014-05-05 13:52:42 +0200

    Conversion of doc to JSDoc

--- ScrollableContainer.js
+++ ScrollableContainer.js
@@ -1,28 +1,34 @@
+/** @module deliteful/ScrollableContainer */
 define([
 	"delite/register",
 	"delite/Container",
 	"delite/Scrollable"
 ], function (register, Container, Scrollable) {
 
-	// module:
-	//		deliteful/ScrollableContainer
-	
-	return register("d-scrollable-container", [HTMLElement, Container, Scrollable], {
-		// summary:
-		//		A container widget with scrolling capabilities.
-		// description:
-		//		A container widget which can scroll its content 
-		//		horizontally and/or vertically. Its scrolling capabilities 
-		//		and API are provided by the mixin delite/Scrollable.
-		// example:
-		//	|	<d-scrollable-container>
-		//	|		<div>...</div>
-		//	|		<div>...</div>
-		//	|		<div>...</div>
-		//	|	</d-scrollable-container>
+	/**
+	 * @summary 
+	 * A container widget with scrolling capabilities.
+	 * @description
+	 * A container widget which can scroll its contents horizontally and/or
+	 * vertically. Its scrolling capabilities and API are provided by the 
+	 * mixin delite/Scrollable.
+	 * @example
+	 * <d-scrollable-container>
+	 *   <div>...</div>
+	 *   <div>...</div>
+	 * </d-scrollable-container>
+	 * @class module:deliteful/ScrollableContainer
+	 * @augments {module:delite/Container}
+	 * @augments {module:delite/Scrollable}
+	 */
+	return register("d-scrollable-container", [HTMLElement, Container, Scrollable],
+		/** @lends module:deliteful/ScrollableContainer# */{
 
-		// baseClass: String
-		//		The name of the CSS class of this widget.
+		/**
+		 * The name of the CSS class of this widget.
+		 * @member {string}
+		 * @default "d-scrollable-container"
+		 */
 		baseClass: "d-scrollable-container"
 	});
 });
