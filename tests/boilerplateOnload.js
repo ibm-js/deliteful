11afc246e7b13474d2d26b88d396687c80a53e7b
Sebastien Brunot <sbrunot@gmail.com>
2014-05-14 16:54:18 +0200

    Fix https://github.com/ibm-js/deliteful/issues/113: deliteful is not sharing test resources with delite anymore, in order to enable testing during the Travis CI build.

---
+++ tests/boilerplateOnload.js
@@ -0,0 +1,4 @@
+// Part of boilerplate.js but need to put it in a separate file for IE9, so it doesn't run until loader has finished
+// loading and require is defined
+/* global boilerplateOnLoad */
+require(["dojo/domReady!"], boilerplateOnLoad);
