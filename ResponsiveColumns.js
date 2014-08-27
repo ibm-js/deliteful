2da6f7978d8b0b69af256709060dea79be1889b3
Damien Mandrioli <dmandrioli@gmail.com>
2014-08-27 10:45:24 +0200

    Add ResponsiveColumns container, Close #269

---
+++ ResponsiveColumns.js
@@ -0,0 +1,205 @@
+/** @module deliteful/ResponsiveColumns */
+define([
+	"dojo/dom-class",
+	"delite/register",
+	"delite/DisplayContainer",
+	"delite/theme!./ResponsiveColumns/themes/{{theme}}/ResponsiveColumns.css"
+], function (domClass, register, DisplayContainer) {
+	/**
+	 * A container that lays out its children according to the screen width. This widget relies on CSS media queries
+	 * (http://www.w3.org/TR/css3-mediaqueries). You can define any number of screen classes by setting the breakpoints
+	 * attribute. Then you must then set the layout attribute on each child to configure a width for each screen class.
+	 * The following example defines two screen classes: "phone" and "other" with a breakpoint at 500px. If the "phone"
+	 * class is active, the first child width is 100% and the second child is hidden. If the screen is larger than 500px
+	 * then the first child width is 20% and the second one fill the remaining space.
+	 * @example:
+	 * <d-responsive-columns breakpoints="{phone: '500px', other: ''}">
+	 *     <div layout="{phone: '100%', other: '20%'}">...</div>
+	 *     <div layout="{phone: 'hidden', other: 'fill'}">...</div>
+	 * </d-responsive-columns>
+	 *
+	 * When the screen width changes and a new screen class is applied by the container, a "change" event is emitted
+	 * with two specific properties: `screenClass` (the new screen class) and `mediaQueryList`
+	 * (the MediaQueryList instance at the origin of the change).
+	 *
+	 * @class module:deliteful/ResponsiveColumns
+	 * @augments module:delite/DisplayContainer
+	 */
+	return register("d-responsive-columns", [HTMLElement, DisplayContainer],
+		{
+			baseClass: "d-responsive-columns",
+
+			/**
+			 * A string containing underlying media queries breakpoints definition. Each breakpoint is made of a
+			 * name (which defines the screen size class) and an upper bound in pixels.
+			 * The upper bound can be a string like "200px", an empty string or a number. The last breakpoint
+			 * which defines the largest screen class is usually set to an empty string to match any size larger
+			 * than the penultimate breakpoint bound. This property is parsed internally by `JSON.parse()`.
+			 * To facilitate writing markup you can use single quotes when defining this property, single quotes
+			 * will be replaced by double quotes before interpreted by `JSON.parse`.
+			 *
+			 * @member {string}
+			 * @default "{'small': '480px', 'medium': '1024px', large: ''}"
+			 */
+			breakpoints: "{'small': '480px', 'medium': '1024px', 'large': ''}",
+
+			/**
+			 * The current screen class currently applied by the container.
+			 * It depends on the value of breakpoints attribute and current screen width. This attribute is read-only.
+			 * When this attribute is modified, a "change" event is emitted which contains the new `screenClass` value.
+			 * @readonly
+			 * @member {string}
+			 */
+			screenClass: "",
+
+			preRender: function () {
+				this._breakpoints = {};
+				this._layouts = [];
+				// A set of MediaQueryList
+				this._mqls = [];
+				// Unique class for this widget
+				domClass.add(this, "-d-responsive-columns-" + this.widgetId);
+			},
+
+			postRender: function () {
+				this.notifyCurrentValue("breakpoints");
+			},
+			
+			_removeListeners: function () {
+				for (var i = 0; i < this._mqls.length; i++) {
+					this._mqls[i].mql.removeListener(this._mqls[i].listener);
+				}
+				this._mqls = [];
+			},
+
+			_checkConfiguration: function () {
+				var result = true;
+				for (var sc in this._breakpoints) {
+					for (var i = 0; i < this._layouts.length; i++) {
+						if (!this._layouts[i][sc]) {
+							result = false;
+							throw new SyntaxError("Warning: ResponsiveColumns, the 'layout' attribute " +
+								"of the child at index " + i + " must define a value for the key '" + sc + "'.");
+						}
+					}
+				}
+				return result;
+			},
+
+			_parseJSONAttrs: function () {
+				var result = true;
+				this._breakpoints = {};
+				this._layouts = [];
+				this._breakpoints = JSON.parse(this.breakpoints.replace(/\'/g, "\""));
+
+				var children = this.getChildren();
+				var layout;
+				for (var i = 0; i < children.length; i++) {
+					layout = children[i].getAttribute("layout");
+					if (!layout) {
+						result = false;
+						throw new SyntaxError("Warning: ResponsiveColumns child at index " + i +
+							", 'layout' is not defined.");
+					} else {
+						layout = JSON.parse(layout.replace(/\'/g, "\""));
+					}
+					if (!layout) {
+						result = false;
+						throw new SyntaxError("Warning: ResponsiveColumns child at index " + i +
+							", 'layout' parsing failed.");
+					} else {
+						this._layouts.push(layout);
+					}
+				}
+				return result;
+			},
+
+			_genCSS: function () {
+				this._removeListeners();
+				var thresholds = [0];
+				var sizeClasses = [];
+				var thr, i;
+				for (var t in this._breakpoints) {
+					sizeClasses.push(t);
+					thr = parseInt(this._breakpoints[t].replace(/px/g, ""), 10);
+					if (thr) {
+						thresholds.push(thr);
+					}
+				}
+
+				var children = this.getChildren();
+				var content = "";
+				var mqHeader = "";
+				var val;
+				var mediaPart = "@media screen and ";
+				var minPart = "(min-width: A)";
+				var maxPart = " and (max-width: B)";
+				var mql;
+				var listener;
+				// Generates media queries blocks with columns layout definition inside
+				for (i = 0; i < thresholds.length; i++) {
+					mqHeader = minPart.replace("A", (thresholds[i] + 1) + "px");
+					if (thresholds[i + 1]) {
+						mqHeader += maxPart.replace("B", (thresholds[i + 1]) + "px");
+					} else {
+						// No upper bounds
+						mqHeader = mqHeader.replace(" and (max-width: B)", "");
+					}
+					content += mediaPart + mqHeader + "{";
+					for (var j = 0; j < children.length; j++) {
+						content += ".-d-responsive-columns-" + this.widgetId + " > *:nth-child(" + (j + 1) + "){";
+						val = this._layouts[j][sizeClasses[i]];
+						if (val === "hidden") {
+							content += "display: none;";
+						} else if (val === "fill") {
+							content += "-webkit-box-flex: 1;";
+							content += "-moz-box-flex: 1;";
+							content += "-webkit-flex: 1;";
+							content += "-ms-flex: 1;";
+							content += "flex: 1;";
+						} else {
+							content += "width: " + val + ";";
+						}
+						content += "}";
+					}
+					content += "}";
+
+					// Listen for breakpoint triggers
+					mql = window.matchMedia(mqHeader);
+					listener = function (a) {
+						if (a.matches) {
+							this.target.screenClass = this.class;
+							this.target.emit("change", {screenClass: this.class, mediaQueryList: mql});
+						}
+					}.bind({class: sizeClasses[i], mql: mql, target: this});
+					mql.addListener(listener);
+					this._mqls.push({mql: mql, listener: listener});
+					// Initialization of screenClass
+					if (!this.screenClass && mql.matches) {
+						this.screenClass = sizeClasses[i];
+					}
+				}
+				var styleBlockId = "d-responsive-columns-generated-style-" + this.widgetId;
+				var styleBlock = this.ownerDocument.getElementById(styleBlockId);
+				if (! styleBlock) {
+					styleBlock = this.ownerDocument.createElement("style");
+					styleBlock.id = styleBlockId;
+					this.ownerDocument.head.appendChild(styleBlock);
+				}
+				styleBlock.innerHTML = content;
+
+			},
+
+			onAddChild: function (/*jshint unused: vars */node) {
+				this.notifyCurrentValue("breakpoints");
+			},
+
+			refreshRendering: function (oldValues) {
+				if ("breakpoints" in oldValues) {
+					if (this._parseJSONAttrs() && this._checkConfiguration()) {
+						this._genCSS();
+					}
+				}
+			}
+		});
+});
---
+++ tests/unit/ResponsiveColumns.js
@@ -0,0 +1,100 @@
+define([
+	"intern!object",
+	"intern/chai!assert",
+	"dojo/dom-class",
+	"deliteful/ResponsiveColumns"
+], function (registerSuite, assert, domClass, ResponsiveColumns) {
+	var container;
+
+	function testLayout(element, targetSize) {
+		var elementStyle = function (key) {
+			if (key === "flex") {
+				var flexAttrs = ["-webkit-box-flex", "-moz-box-flex", "-webkit-flex", "-ms-flex", "flex"];
+				for (var i = 0; i < flexAttrs.length; i++) {
+					var v = window.getComputedStyle(this.node).getPropertyValue(flexAttrs[i]);
+					if (v) {
+						return v.charAt(0);
+					}
+				}
+				return "0";
+			}
+
+			return window.getComputedStyle(this.node).getPropertyValue(key);
+		}.bind({node: element});
+		if (targetSize === "hidden") {
+			assert.strictEqual(elementStyle("display"), "none");
+		} else if (targetSize === "fill") {
+			assert.strictEqual(elementStyle("flex"), "1");
+			assert.notStrictEqual(elementStyle("display"), "none");
+		} else if (targetSize.indexOf("px") !== -1) {
+			assert.strictEqual(elementStyle("width"), targetSize);
+			assert.notStrictEqual(elementStyle("flex"), "1");
+			assert.notStrictEqual(elementStyle("display"), "none");
+		} else if (targetSize.indexOf("%") !== -1) {
+			var w = parseInt(elementStyle("width").replace("px", ""), 10);
+			targetSize = parseInt(targetSize.replace("%", ""), 10);
+			assert.isTrue(Math.abs(w - (window.innerWidth * targetSize / 100)) < 3,
+				"Wrong percent size"); // 3px tolerance
+			assert.notStrictEqual(elementStyle("flex"), "1");
+			assert.notStrictEqual(elementStyle("display"), "none");
+		}
+	}
+
+	registerSuite({
+		name: "ResponsiveColumns",
+		setup: function () {
+			var nospace = document.createElement("style");
+			nospace.innerHTML = "*{padding: 0; margin: 0}";
+			window.document.body.appendChild(nospace);
+
+			container = new ResponsiveColumns();
+
+			window.document.body.appendChild(container);
+			container.breakpoints = "{'small': '500px', 'medium': '900px', 'large': ''}";
+			var child = document.createElement("div");
+			child.setAttribute("layout", "{'small': '100%', 'medium': '200px', 'large': '10%'}");
+			child.innerHTML = "Child 1";
+			container.addChild(child);
+			child = document.createElement("div");
+			child.setAttribute("layout", "{'small': 'hidden', 'medium': 'fill', 'large': '30%'}");
+			child.innerHTML = "Child 2";
+			container.addChild(child);
+			child = document.createElement("div");
+			child.innerHTML = "Child 3";
+			child.setAttribute("layout", "{'small': 'hidden', 'medium': 'hidden', 'large': '60%'}");
+			container.addChild(child);
+			container.startup();
+		},
+
+		"Media Query Test": function () {
+			var iw = window.innerWidth;
+			var targetClass, w1, w2, w3;
+			if (iw < 500) {
+				targetClass = "small";
+				w1 = "100%";
+				w2 = "hidden";
+				w3 = "hidden";
+			} else if (iw < 900) {
+				targetClass = "medium";
+				w1 = "200px";
+				w2 = "fill";
+				w3 = "hidden";
+			} else {
+				targetClass = "large";
+				w1 = "10%";
+				w2 = "30%";
+				w3 = "60%";
+			}
+
+			assert.strictEqual(container.screenClass, targetClass);
+			var children = container.getChildren();
+			testLayout(children[0], w1);
+			testLayout(children[1], w2);
+			testLayout(children[2], w3);
+		},
+
+		teardown: function () {
+			document.body.removeChild(container);
+		}
+	});
+});
