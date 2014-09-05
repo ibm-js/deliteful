9e2a22bc7658ed5d4af4cbaad896cef7ce511449
Patrick Ruzand <pruzand@gmail.com>
2014-09-05 15:27:27 +0200

    Switch inherits from CheckBox, tests refactoring

--- tests/unit/CheckBox.js
+++ tests/unit/CheckBox.js
@@ -4,82 +4,50 @@ define([
 	"intern/chai!assert",
 	"delite/register",
 	"dojo/dom-class",
-	"deliteful/CheckBox"
-], function (dcl, registerSuite, assert, register, domClass, CheckBox) {
+	"deliteful/CheckBox",
+	"./resources/CheckBox-shared"
+], function (dcl, registerSuite, assert, register, domClass, CheckBox, commonSuite) {
 
 	var container,
 		html = "<d-checkbox id='cb1'></d-checkbox><d-checkbox id='cb2' value='foo'>" +
-			"</d-checkbox><d-checkbox id='cb3' checked></d-checkbox><label id='lbl4' for='cb3'>cb3</label>";
+			"</d-checkbox><d-checkbox id='cb3' checked='true'></d-checkbox><label id='lbl4' for='cb3'>cb3</label>";
 
-	var commonSuite = {
 
-		"Default State": function () {
-			var cb = document.getElementById("cb1");
-			assert.isTrue(domClass.contains(cb, "d-checkbox"), "Unexpected baseClass.");
-			assert.isFalse(cb.checked, "Unexpected default value for 'checked' property.");
-			assert.isFalse(cb.disabled, "Unexpected default value for 'disabled' property");
-			assert.strictEqual(cb.value, "on", "Unexpected default value for 'value' property");
-			var elt = cb.querySelector("input[type='checkbox']");
-			assert.ok(elt, "Missing wrapped input element.");
-
-			// verify properties bounds in the template
-			assert.strictEqual(cb.name, elt.name, "Unexpected value for wrapped input 'name' property.");
-			assert.strictEqual(cb.value, elt.value, "Unexpected value for wrapped input 'value' property.");
-
+	var suite = {
+		setup: function () {
+			dcl.mix(commonSuite, {
+				baseClass: "d-checkbox",
+				defaultWidget: "cb1",
+				labelForTarget: "cb3"
+			});
+		},
+		"initState": function () {
 			var cb2 = document.getElementById("cb2");
 			assert.strictEqual(cb2.value, "foo",
 				"Unexpected default value for 'value' property if 'value' specified/unchecked");
-			cb = document.getElementById("cb3");
+			var cb = document.getElementById("cb3");
 			assert.ok(cb.checked, "Unexpected default value for 'checked' property if 'checked' specified.");
 		},
-
-		"checked": function () {
-			var cb1 = document.getElementById("cb1"),
-				inp = cb1.firstChild,
-				old = cb1.checked;
-			cb1.checked = !old;
-			assert.strictEqual(cb1.checked, !old, "Unexpected value for 'checked' property after set.");
-			//sync template
-			cb1.deliver();
-			assert.strictEqual(cb1.checked, inp.checked, "Unexpected value for 'checked' property of wrapped input.");
-		},
-
-		value: function () {
-			var cb = document.getElementById("cb1");
-			cb.value = "foo";
-			//sync template
-			cb.deliver();
-			assert.strictEqual(cb.firstChild.getAttribute("value"), "foo", "Unexpected value for 'value' attribute.");
-		},
-
-		"labelFor": function () {
-			var cb3 = document.getElementById("cb3"),
-				lbl4 = document.getElementById("lbl4");
-			assert.strictEqual(cb3._lbl4, lbl4, "Unexpected labelFor elt associated with cb3.");
-			lbl4.click();
-			cb3.deliver();
-			assert.isFalse(cb3.checked, "Unexpected value for 'checked' property after labelFor click.");
-		},
-
 		afterEach: function () {
 			container.parentNode.removeChild(container);
 		}
 	};
+	dcl.mix(suite, commonSuite.testCases);
 
 	// Markup
-	var suite = {
+	var markupSuite = {
 		name: "deliteful/CheckBox: markup",
-		beforeEach: function () {
+		"beforeEach": function () {
 			container = document.createElement("div");
 			document.body.appendChild(container);
 			container.innerHTML = html;
 			register.parse();
 		}
 	};
-	dcl.mix(suite, commonSuite);
-	registerSuite(suite);
+	dcl.mix(markupSuite, suite);
+	registerSuite(markupSuite);
 
-	suite = {
+	var progSuite = {
 		name: "deliteful/CheckBox: programmatic",
 		beforeEach: function () {
 			container = document.createElement("div");
@@ -95,11 +63,11 @@ define([
 			lbl4.setAttribute("for", "cb3");
 			lbl4.textContent = "cb3";
 			container.appendChild(lbl4);
-			cb = new CheckBox({id: "cb3", checked: "checked"});
+			cb = new CheckBox({id: "cb3", checked: true});
 			container.appendChild(cb);
 			cb.startup();
 		}
 	};
-	dcl.mix(suite, commonSuite);
-	registerSuite(suite);
+	dcl.mix(progSuite, suite);
+	registerSuite(progSuite);
 });
---
+++ tests/unit/resources/CheckBox-shared.js
@@ -0,0 +1,82 @@
+define([
+	"../../../../dcl/dcl",
+	"intern!object",
+	"intern/chai!assert",
+	"delite/register",
+	"dojo/dom-class"
+], function (dcl, registerSuite, assert, register, domClass) {
+
+	var commonSuite = {
+		defaultWidget: "",
+		baseClass: "",
+		labelForTarget: ""
+	};
+
+	commonSuite.testCases = {
+		"Default State": function () {
+			var d = this.async(1000);
+			setTimeout(d.callback(function () {
+				var cb = document.getElementById(commonSuite.defaultWidget);
+				assert.isTrue(domClass.contains(cb, commonSuite.baseClass), "Unexpected baseClass.");
+				assert.isFalse(cb.checked, "Unexpected default value for 'checked' property.");
+				assert.isFalse(cb.disabled, "Unexpected default value for 'disabled' property");
+				assert.strictEqual(cb.value, "on", "Unexpected default value for 'value' property");
+				var elt = cb.querySelector("input[type='checkbox']");
+				assert.ok(elt, "Missing wrapped input element.");
+
+				// verify properties bounds in the template
+				assert.strictEqual(cb.name, elt.name, "Unexpected value for wrapped input 'name' property.");
+				assert.strictEqual(cb.value, elt.value, "Unexpected value for wrapped input 'value' property.");
+			}), 300);
+			return d;
+		},
+
+		"checked": function () {
+			var cb1 = document.getElementById(commonSuite.defaultWidget),
+				inp = cb1.firstChild,
+				old = cb1.checked;
+			cb1.checked = !old;
+			assert.strictEqual(cb1.checked, !old, "Unexpected value for 'checked' property after set.");
+			//sync template
+			cb1.deliver();
+			assert.strictEqual(cb1.checked, inp.checked, "Unexpected value for 'checked' property of wrapped input.");
+		},
+
+		value: function () {
+			var cb = document.getElementById(commonSuite.defaultWidget);
+			cb.value = "foo";
+			//sync template
+			cb.deliver();
+			assert.strictEqual(cb.firstChild.getAttribute("value"), "foo", "Unexpected value for 'value' attribute.");
+		},
+
+		"labelFor": function () {
+			var d = this.async(1000);
+			setTimeout(d.callback(function () {
+				var cb3 = document.getElementById(commonSuite.labelForTarget),
+					lbl4 = document.getElementById("lbl4"),
+					old = cb3.checked;
+				lbl4.click();
+				cb3.deliver();
+				assert.strictEqual(!old, cb3.checked, "Unexpected value for 'checked' property after labelFor click.");
+			}), 300);
+			return d;
+		},
+
+		"changeEvent": function () {
+			var cb3 = document.getElementById(commonSuite.labelForTarget),
+				lbl4 = document.getElementById("lbl4"),
+				fired = false;
+			cb3.on("change", function () {
+				fired = true;
+			});
+			cb3.focusNode.click();
+			assert.isTrue(fired, "Missing 'change' event when input node is clicked.");
+			fired = false;
+			lbl4.click();
+			assert.isTrue(fired, "Missing 'change' event when labelFor is clicked.");
+		}
+	};
+
+	return commonSuite;
+});
