a184f2401e736da4ec8000d8a2496d345f65c5db
Semion Chichelnitsky <semion@il.ibm.com>
2014-12-01 13:52:15 +0200

    Bidi support in deliteful/Button

--- Button/bidi/Button.js
+++ Button/bidi/Button.js
@@ -12,10 +12,12 @@ define(["dcl/dcl"], function (dcl) {
 		//		This class should not be used directly.
 		//		Button widget loads this module when user sets "has: {'bidi': true }" in data-dojo-config.
 
-		refreshRendering: function () {
-			this.containerNode.textContent = this.wrapWithUcc(this.containerNode.textContent);
-			if (this.title) {
-				this.title = this.wrapWithUcc(this.title);
+		refreshRendering: function (oldVals) {
+			if ("textDir" in oldVals || "label" in oldVals) {
+				this.labelNode.textContent = this.applyTextDirection(this.label);
+				if (this.title) {
+					this.title = this.applyTextDirection(this.title);
+				}				
 			}
 		}
 	});
