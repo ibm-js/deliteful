48fb5780ea08ba0074f0597da1f3b2a26b54b460
edurocher <edurocher0@gmail.com>
2014-12-03 09:41:33 +0100

    Added SwapView / ViewIndicator unit and functional tests.

---
+++ tests/unit/SwapView.js
@@ -0,0 +1,69 @@
+define([
+	"intern!object",
+	"intern/chai!assert",
+	"dojo/dom-geometry",
+	"dojo/dom-class",
+	"delite/register",
+	"deliteful/SwapView"
+], function (registerSuite, assert, domGeom, domClass, register) {
+	var container, node;
+	var aaa, bbb, ccc, ddd;
+	var asyncHandler;
+	var htmlContent = "<d-swap-view id='vs'><div id='aaa'>AAA</div><div id='bbb'>BBB</div><div id='ccc'>CCC</div>" +
+		"<div id='ddd'>DDD</div></d-swap-view>";
+
+	function checkNodeVisibility(vs, target) {
+		for (var i = 0; i < vs.children.length; i++) {
+			assert.isTrue(
+				((vs.children[i] === target && vs.children[i].style.display !== "none" &&
+					vs.selectedChildId === target.id)) ||
+					(vs.children[i] !== target && vs.children[i].style.display === "none")
+			);
+		}
+	}
+	registerSuite({
+		name: "SwapView Markup",
+		setup: function () {
+			container = document.createElement("div");
+			document.body.appendChild(container);
+			container.innerHTML = htmlContent;
+			register.parse(container);
+			node = document.getElementById("vs");
+			aaa = document.getElementById("aaa");
+			bbb = document.getElementById("bbb");
+			ccc = document.getElementById("ccc");
+			ddd = document.getElementById("ddd");
+		},
+		"Default CSS" : function () {
+			assert.isTrue(domClass.contains(node, "d-swap-view"));
+			assert.isTrue(domClass.contains(node, "d-view-stack"));
+		},
+		"Default values" : function () {
+			assert.deepEqual(node.transition, "slide");
+			assert.deepEqual(node.reverse, false);
+		},
+		"Show (by widget)" : function () {
+			var d = this.async(1000);
+			asyncHandler = node.on("delite-after-show", d.callback(function () {
+				checkNodeVisibility(node, bbb);
+			}));
+			node.show(bbb);
+		},
+		"Show (by id)" : function () {
+			var d = this.async(1000);
+			asyncHandler = node.on("delite-after-show", d.callback(function () {
+				checkNodeVisibility(node, aaa);
+			}));
+			node.show("aaa");
+		},
+
+		teardown: function () {
+			container.parentNode.removeChild(container);
+		},
+		afterEach: function () {
+			if (asyncHandler) {
+				asyncHandler.remove();
+			}
+		}
+	});
+});
