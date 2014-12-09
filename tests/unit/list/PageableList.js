d6945932812598420e1b72ebe543c5a6d5235aa4
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-12-09 14:27:43 +0100

    renabled testing for ie10, fixes #280

--- tests/unit/list/PageableList.js
+++ tests/unit/list/PageableList.js
@@ -12,13 +12,6 @@ define([
 
 	var Store = Memory.createSubclass([Trackable], {});
 
-	// PageableList is currently not supported on IE10
-	// see https://github.com/ibm-js/deliteful/issues/280
-	if (has("ie") > 9 && has("ie") < 11) {
-		console.log("Skipping PageableList tests on IE10 (see https://github.com/ibm-js/deliteful/issues/280)");
-		return;
-	}
-
 	/////////////////////////////////
 	// List base tests
 	/////////////////////////////////
