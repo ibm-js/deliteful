a50510dcc49556068f60121653d162f4f42b7e21
Semion Chichelnitsky <semion@il.ibm.com>
2014-12-17 11:08:45 +0200

    Squashed commit of the following:
    commit 8870ec1579ca087fa81fd7ed7ec5c79776642453
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Tue Dec 16 09:57:38 2014 +0200
    
        Control textDir in ToggleButton
    
    commit e09a451ad0154098e09bcdde3e2a6de32320779f
    Merge: 1d7adc3 462ae09
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Tue Dec 16 09:52:19 2014 +0200
    
        Merge remote-tracking branch 'upstream/master'
    
    commit 1d7adc371ab0220cf9f66460254153a1049c03f9
    Merge: 6673cfc a81ee98
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Mon Dec 15 17:11:56 2014 +0200
    
        Merge remote-tracking branch 'upstream/master'
    
    commit 6673cfccd90a55ab87fc70bb4bb7ac2621391176
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Mon Dec 15 16:46:25 2014 +0200
    
        Remove dpointer from parameters
    
    commit 2d7f7bf45623e8b2980eb2789fce1fe5995511fb
    Merge: 5ffb335 01818e0
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Thu Dec 11 10:39:27 2014 +0200
    
        Merge remote-tracking branch 'upstream/master'
    
    commit 5ffb3352fe6551b449608fcfab83fd9000ad0bd3
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Thu Dec 11 10:09:05 2014 +0200
    
        Delete check for title in oldVals
    
    commit c213cd56bed63c0aa3356df2570a65cc9ff943d9
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Thu Dec 4 16:39:38 2014 +0200
    
        Fix jshint errors
    
    commit 9c32f020979f83e6e05e4ab135e79774d2e77fcd
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Thu Dec 4 16:30:36 2014 +0200
    
        Refresh title
    
    commit a184f2401e736da4ec8000d8a2496d345f65c5db
    Author: Semion Chichelnitsky <semion@il.ibm.com>
    Date:   Mon Dec 1 13:52:15 2014 +0200
    
        Bidi support in deliteful/Button

--- ToggleButton/bidi/ToggleButton.js
+++ ToggleButton/bidi/ToggleButton.js
@@ -1,12 +1,30 @@
 define([
-	"dcl/dcl"
+	"dcl/dcl", "dpointer/events"
 ], function (dcl) {
 
 	return dcl(null, {
-		_setCheckedLabelAttr: function (value) {
-			value = this.wrapWithUcc(value);
-			this._set("checkedLabel", value);
-		}
+		refreshRendering: function (oldVals) {
+			if (this.checked && this.checkedLabel) {
+				if ("checked" in oldVals || "textDir" in oldVals || "checkedLabel" in oldVals) {
+					this.labelNode.textContent = this.applyTextDirection(this.checkedLabel);
+				}
+			}
+			else if ("checked" in oldVals || "textDir" in oldVals || "label" in oldVals) {
+				this.labelNode.textContent = this.applyTextDirection(this.label);
+			}
+			if (this.title && "textDir" in oldVals) {
+				this.title = this.applyTextDirection(this.title);
+			}
+		},
 
+		postRender: function () {
+			this.on("pointerover", this._pointerOverHandler.bind(this));
+		},
+
+		_pointerOverHandler: function (evt) {
+			if (this.title) {
+				this.title = this.applyTextDirection(this.title);
+			}
+		}
 	});
 });
