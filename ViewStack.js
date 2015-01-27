d89fc0f046bfd0944567fa1306838839f21f2af1
Damien Mandrioli <dmandrioli@gmail.com>
2015-01-27 13:53:03 +0100

    Viewstack jshint fix

--- ViewStack.js
+++ ViewStack.js
@@ -156,9 +156,9 @@ define(["dcl/dcl",
 			/**
 			 * Shows the immediately following sibling of the ViewStack visible element.
 			 * The parameter 'params' is optional. If not specified, this.transition, and this.reverse are used.
-			 * @param {Object} [params] - Optional params. A hash like {transition: "reveal", reverse: true}. The transition value
-			 * can be "slide", "overlay", "fade" or "flip". Reverse transition applies to "slide" and
-			 * "reveal". Transition is internally set to "none" if the ViewStack is not visible.
+			 * @param {Object} [params] - Optional params. A hash like {transition: "reveal", reverse: true}.
+			 * The transition value can be "slide", "overlay", "fade" or "flip". Reverse transition applies to "slide"
+			 * and "reveal". Transition is internally set to "none" if the ViewStack is not visible.
 			 * @returns {Promise} A promise that will be resolved when the display and transition effect will have
 			 * been performed.
 			 */
