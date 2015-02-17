1ca6f41dc923c50ad377d1b3289e15b2cd7f48b5
Semion Chichelnitsky <semion@il.ibm.com>
2015-02-17 16:05:56 +0200

    Update bidi/Switch 2

--- Switch/bidi/Switch.js
+++ Switch/bidi/Switch.js
@@ -5,11 +5,14 @@ define([
 
 	return dcl(null, {
 		refreshRendering: function (oldVals) {
-			if ("textDir" in oldVals || "checkedLabel" in oldVals) {
+			if ("dir" in oldVals || "textDir" in oldVals || "checkedLabel" in oldVals) {
 				this._innerNode.firstChild.textContent = this.applyTextDirection(this.checkedLabel);
 			}
-			if ("textDir" in oldVals || "uncheckedLabel" in oldVals) {
+			if ("dir" in oldVals || "textDir" in oldVals || "uncheckedLabel" in oldVals) {
 				this._innerNode.lastChild.textContent = this.applyTextDirection(this.uncheckedLabel);
+			}
+			if (this.title && "textDir" in oldVals) {
+				this.title = this.applyTextDirection(this.title);
 			}
 		},
 
