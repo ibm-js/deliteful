f1757c2a84c1990aa2d50970191ffd6c3e17f48b
Bill Keese <bill@dojotoolkit.org>
2016-07-21 10:48:28 +0900

    Remove <label> click handler from Checkbox. Instead, code should use <label for="cb-input"> (to label a checkbox with id=cb).
    Refs ibm-js/delite#448.

--- Checkbox.js
+++ Checkbox.js
@@ -8,8 +8,6 @@ define([
 	"delite/theme!./Checkbox/themes/{{theme}}/Checkbox.css"
 ], function (dcl, register, FormWidget, Toggle, template) {
 
-	var labelClickHandler;
-
 	/**
 	 * A 2-state checkbox widget similar to an HTML5 input type="checkbox" element.
 	 * @example
@@ -37,26 +35,6 @@ define([
 
 		_inputClickHandler: function () {
 			this.checked = this.focusNode.checked;
-		},
-
-		attachedCallback: function () {
-			// The fact that deliteful/Checkbox is not an HTMLInputElement seems not being compatible with the default
-			// "<label for" behavior of the browser. So it needs to explicitly listen to click on associated
-			// <label for=...> elements.
-			if (!labelClickHandler) {
-				// set a global listener that listens to click events on label elt
-				labelClickHandler = function (e) {
-					var forId;
-					if (/label/i.test(e.target.tagName) && (forId = e.target.getAttribute("for"))) {
-						var elt = document.getElementById(forId);
-						if (elt && elt.render && elt._lbl4 !== undefined) { //_lbl4: to check it's a checkbox
-							// call click() on the input instead of this.toggle() to get the 'change' event for free
-							elt.focusNode.click();
-						}
-					}
-				};
-				this.ownerDocument.addEventListener("click", labelClickHandler);
-			}
 		}
 	});
 });
--- tests/unit/Checkbox.js
+++ tests/unit/Checkbox.js
@@ -9,7 +9,8 @@ define([
 
 	var container,
 		html = "<d-checkbox id='cb1'></d-checkbox><d-checkbox id='cb2' value='foo'>" +
-			"</d-checkbox><d-checkbox id='cb3' checked='true'></d-checkbox><label id='lbl4' for='cb3'>cb3</label>";
+			"</d-checkbox><d-checkbox id='cb3' checked='true'></d-checkbox>" +
+			"<label id='lbl4' for='cb3-input'>cb3</label>";
 
 	var suite = {
 		setup: function () {
@@ -59,7 +60,7 @@ define([
 			cb.attachedCallback();
 			var lbl4 = document.createElement("label");
 			lbl4.id = "lbl4";
-			lbl4.setAttribute("for", "cb3");
+			lbl4.setAttribute("for", "cb3-input");
 			lbl4.textContent = "cb3";
 			container.appendChild(lbl4);
 			cb = new Checkbox({id: "cb3", checked: true});
