5c59f81fbe60dbc126b3c01c876800efdca65fb1
Bill Keese <bill@dojotoolkit.org>
2016-07-21 11:10:15 +0900

    Fix spurious IE11 Checkbox test failure, and general test cleanup.
    Theoretically the polling should be better than sleep(400) calls
    but the error messages it produces are really obscure.

--- tests/functional/Checkbox.js
+++ tests/functional/Checkbox.js
@@ -17,62 +17,55 @@ define([
 	registerSuite({
 		name: "Checkbox - functional",
 
-		"Checkbox behavior": function () {
+		"mouse": function () {
 			var remote = this.remote;
 			return loadFile(remote, "./Checkbox.html")
 				// default click action
-				.execute("return document.getElementById('cb1').focusNode;")
-				.then(function (element) {
-					element.click();
+				.sleep(400)
+				.execute("return document.getElementById('cb1').checked;").then(function (checked) {
+					assert.isFalse(checked, "cb1 initially unchecked");
 				})
-				.then(pollUntil("return document.getElementById('cb1').checked ? true : null;", [],
-						intern.config.WAIT_TIMEOUT, intern.config.POLL_INTERVAL))
-				// click on disabled checkbox
-				.then(pollUntil("return document.getElementById('cb2').checked ? null : true;", [],
-						intern.config.WAIT_TIMEOUT, intern.config.POLL_INTERVAL))
-				.execute("return document.getElementById('cb2').focusNode;")
-				.then(function (element) {
-					element.click();
+				.findByCssSelector("#cb1 input").click().end()
+				.sleep(400)
+				.execute("return document.getElementById('cb1').checked;").then(function (checked) {
+					assert.isTrue(checked, "click of cb1");
 				})
-				.then(pollUntil("return document.getElementById('cb2').checked ? null : true;", [],
-						intern.config.WAIT_TIMEOUT, intern.config.POLL_INTERVAL));
+				// then click disabled checkbox
+				.findByCssSelector("#cb2 input").click().end()
+				.sleep(400)
+				.execute("return document.getElementById('cb2').checked;").then(function (checked) {
+					assert.isFalse(checked, "click of cb2");
+				});
 		},
 
-		"Checkbox Key nav": function () {
-			// keyb nav
+		"keyboard": function () {
+			// keyboard nav
 			// give the focus to the button to have a ref starting point in the chain
 			var remote = this.remote;
 			if (remote.environmentType.brokenSendKeys || !remote.environmentType.nativeEvents) {
 				return this.skip("no keyboard support");
 			}
-			return remote
+			return loadFile(remote, "./Checkbox.html")
 				.execute("return document.getElementById('b1').focus();")
-				.getActiveElement()
-				.end()
-				.sleep(400)
 				.pressKeys(keys.TAB) // Press TAB -> cb1
-				.sleep(400)
-				.getActiveElement()
-				.getAttribute("name")
-				.then(function (v) {
-					assert.strictEqual(v, "cb1", "Unexpected focused element after 1st TAB.");
-				})
-				.end()
+				.execute("return document.activeElement.getAttribute('name')")
+					.then(function (v) {
+						assert.strictEqual(v, "cb1", "focused element after 1st TAB.");
+					})
 				.pressKeys(keys.SPACE) // Press Space to check cb1
-				.then(pollUntil("return document.getElementById('cb1').checked ? null : true;", [],
-						intern.config.WAIT_TIMEOUT, intern.config.POLL_INTERVAL))
-				.end()
-				.pressKeys(keys.TAB) // Press TAB -> skip cb2 (disabled)
 				.sleep(400)
-				.getActiveElement()
-				.getVisibleText()
-				.then(function (v) {
-					assert.strictEqual(v, "End", "Unexpected focused element after 2nd TAB.");
+				.execute("return document.getElementById('cb1').checked;").then(function (checked) {
+					assert.isTrue(checked, "keyboard click of cb1");
 				})
+				.pressKeys(keys.TAB) // Press TAB -> skip cb2 (disabled)
+				.execute("return document.activeElement.textContent")
+					.then(function (v) {
+						assert.strictEqual(v, "End", "focused element after 2nd TAB.");
+					})
 				;
 		},
 
-		"Checkbox Form": function () {
+		"form": function () {
 			//
 			// Form tests
 			//
@@ -82,32 +75,31 @@ define([
 			}
 			return loadFile(remote, "./Checkbox.html")
 				.findById("form1")
-				.submit()
-				.end()
+					.submit()
+					.end()
 				.setFindTimeout(intern.config.WAIT_TIMEOUT)
-				.find("id", "parameters")
+				.find("id", "parameters")	// TODO: shouldn't this be checking something?
 				.end()
 				.execute("return document.getElementById('valueFor_cb3');")
 				.then(function (value) {
 						assert.isNull(value, "Unexpected value for unchecked checkbox cb3.");
 					})
-				.end()
 				.findById("valueFor_cb4")
-				.getVisibleText()
-				.then(function (value) {
-					assert.strictEqual(value, "2", "Unexpected value for checkbox cb4");
-				})
-				.end()
+					.getVisibleText()
+					.then(function (value) {
+						assert.strictEqual(value, "2", "Unexpected value for checkbox cb4");
+					})
+					.end()
 				.execute("return document.getElementById('valueFor_cb5');")
 				.then(function (value) {
 					assert.isNull(value, "Unexpected value for disabled checkbox cb5.");
 				})
 				.findById("valueFor_cb6")
-				.getVisibleText()
-				.then(function (value) {
-					assert.strictEqual(value, "4", "Unexpected value for checkbox cb6");
-				})
-				;
+					.getVisibleText()
+					.then(function (value) {
+						assert.strictEqual(value, "4", "Unexpected value for checkbox cb6");
+					})
+					.end();
 		}
 	});
 });
