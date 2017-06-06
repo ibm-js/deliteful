4538eda552dc4c5c71d7659a65a5d2c3a988aec4
Bill Keese <bill@dojotoolkit.org>
2017-06-06 23:14:41 +0900

    Fix and clean up the features test.
    On FF53, the platform name is in environmentType.platformName, not
    environmentType.platform.
    
    Also, the testChannelFlagsDone variable didn’t work since it’s
    tantamount to a global.  When the test ran on Firefox is didn’t set
    testChannelFlagsDone, but the test didn’t fail because that flag had
    already been set when the test ran against Chrome.

--- tests/functional/features.js
+++ tests/functional/features.js
@@ -6,33 +6,30 @@ define([
 	"require"
 ], function (intern, registerSuite, pollUntil, assert, require) {
 	
-	var testChannelFlagsDone = false;
-	
-	var loadFile = function (remote, fileName) {
+	function loadFile(remote, fileName) {
 		return remote
 			.get(require.toUrl(fileName))
 			.then(pollUntil("return ready ? true : null;", [],
 				intern.config.WAIT_TIMEOUT, intern.config.POLL_INTERVAL));
-	};
+	}
 	
-	var checkChannelFlags = function (has, isPhone, isTablet, isDesktop, description) {
+	function checkChannelFlags(has, isPhone, isTablet, isDesktop, description) {
 		assert.strictEqual(has["phone-like-channel"], isPhone,
 			"phone-like-channel on " + description);
 		assert.strictEqual(has["tablet-like-channel"], isTablet,
 			"tablet-like-channel on " + description);
 		assert.strictEqual(has["desktop-like-channel"], isDesktop,
 			"desktop-like-channel on " + description);
-		testChannelFlagsDone = true;
-	};
-	var checkChannelFlagsPhone = function (has, description) {
+	}
+	function checkChannelFlagsPhone(has, description) {
 		checkChannelFlags(has, true, false, false, description);
-	};
-	var checkChannelFlagsTablet = function (has, description) {
+	}
+	function checkChannelFlagsTablet(has, description) {
 		checkChannelFlags(has, false, true, false, description);
-	};
-	var checkChannelFlagsDesktop = function (has, description) {
+	}
+	function checkChannelFlagsDesktop(has, description) {
 		checkChannelFlags(has, false, false, true, description);
-	};
+	}
 	
 	registerSuite({
 		name: "deliteful/features - functional",
@@ -43,54 +40,32 @@ define([
 				.execute("return _has")
 				.then(function (has) {
 					/* jshint maxcomplexity: 21 */
-					var platform = remote.environmentType.platform ?
-							remote.environmentType.platform.toUpperCase() : null,
-						deviceName = remote.environmentType.deviceName ?
-							remote.environmentType.deviceName.toUpperCase() : null,
-						browserName = remote.environmentType.browserName ?
-							remote.environmentType.browserName.toUpperCase() : null;
+					var platform = remote.environmentType.platformName || remote.environmentType.platform;
+					var deviceName = remote.environmentType.deviceName;
+					var browserName = remote.environmentType.browserName;
 
 					var description = "platform: " + platform + " deviceName: " + deviceName +
 						" browserName: " + browserName;
-					
-					if (platform &&
-						platform.indexOf("WINDOWS") !== -1 ||
-						platform.indexOf("XP") !== -1 ||
-						platform.indexOf("LINUX") !== -1 ||
-						platform.indexOf("OS X") !== -1 ||
-						(platform.indexOf("MAC") !== -1 &&
-							(!deviceName ||
-								(deviceName.indexOf("IPHONE") === -1 &&
-								deviceName.indexOf("IPAD") === -1)) &&
-							(!browserName || browserName.indexOf("IOS") === -1))) {
+
+					if (/windows|xp|linux|os x|mac/i.test(platform)) {
 						checkChannelFlagsDesktop(has, description);
-					} else if (deviceName) {
+					} else if (/iphone|galaxy s|galaxy note|nexus 4|nexus 5/i.test(deviceName)) {
 						// Values of deviceName may evolve depending on the configuration
 						// of saucelabs test platforms, and depending on the config files
 						// (tests/intern(.local).js).
-						if (deviceName.indexOf("IPHONE") !== -1 ||
-							deviceName.indexOf("GALAXY S") !== -1 ||
-							deviceName.indexOf("GALAXY NOTE") !== -1 ||
-							deviceName.indexOf("NEXUS 4") !== -1 ||
-							deviceName.indexOf("NEXUS 5") !== -1) {
-							checkChannelFlagsPhone(has, description);
-						} else if (deviceName.indexOf("IPAD") !== -1 ||
-							deviceName.indexOf("NEXUS 7") !== -1 ||
-							deviceName.indexOf("NEXUS 9") !== -1 ||
-							deviceName.indexOf("GALAXY TAB") !== -1) {
-							checkChannelFlagsTablet(has, description);
-						}
+						checkChannelFlagsPhone(has, description);
+					} else if (/ipad|nexus 7|nexus 9|galaxy tab/i.test(deviceName)) {
+						checkChannelFlagsTablet(has, description);
+					} else {
+						// Did the test run on a platform such that the testing code above doesn't check
+						// the flags? This throw means we need to update the testing code to add new platforms/devices.
+						throw new Error(
+							"not checked with platform: " + platform +
+							" deviceName: " + deviceName +
+							" browserName: " + browserName +
+							" (new platform/device to be added to the test?)");
 					}
-					// Did the test run on a platform such that the testing code above doesn't check
-					// the flags? This assertion allows to detect whether we need to update the
-					// testing code to add new platforms/devices.
-					assert.isTrue(testChannelFlagsDone,
-						"not checked with platform: " + platform +
-						" deviceName: " + deviceName +
-						" browserName: " + browserName +
-						" (new platform/device to be added to the test?)");
-				})
-				.end();
+				});
 		}
 	});
 });
