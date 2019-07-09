f17a84e7b8086953489d8ef1edd2fc4e5bcd5e71
Bill Keese <bill@dojotoolkit.org>
2019-07-09 09:51:31 +0900

    For VoiceOver to work, use tablet mode even on wide mobile screens. This is to address iPad in landscape and iPad Pro.
    Refs #718.

--- features.js
+++ features.js
@@ -2,23 +2,22 @@
  * This module leverages `requirejs-dplugins/has` and sets `has()` flags that can be
  * used by multichannel widgets to determine the required channel:
  *
- * - `has("phone-like-channel")`: `true` for small screens, `false` otherwise.
- * - `has("tablet-like-channel")`: `true` for medium screens, `false` otherwise.
- * - `has("desktop-like-channel")`: `true` for large screens, `false` otherwise.
- * 
- * These flags are set depending on the screen size, using CSS media queries that
+ * - `has("phone-like-channel")`: `true` for mobile devices with small screens, `false` otherwise.
+ * - `has("tablet-like-channel")`: `true` for mobile devices bigger than phones, `false` otherwise.
+ * - `has("desktop-like-channel")`: `true` for non-mobile devices, `false` otherwise.
+ *
+ * In general pages should dynamically adjust to the browser viewport size, rather than relying
+ * on these static has() flags.  They are only used by Combobox.
+ *
+ * The phone vs. mobile flags are set depending on the screen size, using CSS media queries that
  * compare the actual screen width (the `device-width` media feature) with the corresponding
- * breakpoint values provided by `deliteful/channelBreakpoints`. 
- * 
- * Note that the screen size is the only criteria used for determining
- * the channel. When a channel flag is set to `true`, the other channel flags are set
- * to `false`.
- * 
+ * breakpoint values provided by `deliteful/channelBreakpoints`.
+ *
  * The default values of the breakpoints can be configured using `require.config()`.
  * For details, see the documentation of `deliteful/channelBreakpoints`.
- * 
+ *
  * The channel can be configured statically using `require.config()`, for instance:
- * 
+ *
  * ```html
  * &lt;script>
  *   // configuring RequireJS
@@ -35,45 +34,39 @@
  * &lt;/script>
  * ```
  * Note that only one channel flag should be set to `true`.
- * 
+ *
  * The module returns the `has()` function returned by the module `requirejs-dplugins/has`.
- * 
+ *
  * @module deliteful/features
  */
-define(["requirejs-dplugins/has", "deliteful/channelBreakpoints"],
-	function (has, channelBreakpoints) {
-
-	// Notes:
-	// - Use the device-width media feature rather than width, such that, on
-	// desktop/laptop, the selected channel does not depend on the actual size of the
-	// viewport. Thus, the selected channel depends only on the static characteristics
-	// of the device (its screen width), which fits the use-case of multichannel
-	// widgets that need a statically determined channel. Otherwise it would be confusing
-	// to get a different channel depending on whether the app is initially loaded
-	// in a small or large viewport.
-	// - We do not technically enforce the "small" breakpoint to be smaller than the
-	// medium one. Hence the apparently redundant checks of both media queries
-	// for the small and large channels.
-	
+define([
+	"requirejs-dplugins/has",
+	"deliteful/channelBreakpoints"
+], function (
+	has,
+	channelBreakpoints
+) {
 	// The build system evaluates the function of plugin modules while running
 	// in nodejs. Hence the need to test for presence of window. The value of
 	// the has-features does not matter at build time. (#512)
 	if (typeof window !== "undefined") {
+		// Determine mobile or desktop by sniffing, rather than testing for TouchEvent etc.,
+		// as the latter will be true on desktop machines with touch screens.
+		// See https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device
+		var mobile = /Mobi|Android/i.test(navigator.userAgent);
+
 		// matched by screens at least as large as the "small" breakpoint
 		var mqAboveSmall = window.matchMedia("(min-device-width: " +
 			channelBreakpoints.smallScreen + ")");
-		// matched by screens at least as large as the "medium" breakpoint
-		var mqAboveMedium = window.matchMedia("(min-device-width: " +
-			channelBreakpoints.mediumScreen + ")");
-	
+
 		has.add("phone-like-channel", function () {
-			return !mqAboveSmall.matches && !mqAboveMedium.matches;
+			return mobile && !mqAboveSmall.matches;
 		});
 		has.add("tablet-like-channel", function () {
-			return mqAboveSmall.matches && !mqAboveMedium.matches;
+			return mobile && mqAboveSmall.matches;
 		});
 		has.add("desktop-like-channel", function () {
-			return mqAboveSmall.matches && mqAboveMedium.matches;
+			return !mobile;
 		});
 	}
 
