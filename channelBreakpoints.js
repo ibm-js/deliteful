41e0f87d6239b5a39d73bbfac2f7047f17990e0d
Bill Keese <wkeese@jp.ibm.com>
2020-05-11 12:19:13 +0900

    Remove AMD-specific way to configure channelBreakpoints.
    Instead, application code can alias deliteful/channelBreakpoints
    to its own module.  (This can be done both in Webpack and RequireJS.)

--- channelBreakpoints.js
+++ channelBreakpoints.js
@@ -1,8 +1,5 @@
 /** @module deliteful/channelBreakpoints */
-define([ "module" ], function (module) {
-
-	var config = module.config();
-
+define({
 	/**
 	 * This module returns an object containing properties that define values for breakpoints
 	 * of CSS media queries based on screen size:
@@ -16,43 +13,24 @@ define([ "module" ], function (module) {
 	 * for setting the `has()`-flags `"phone-like-channel"`, `"tablet-like-channel"`, and
 	 * `"desktop-like-channel"`.
 	 *
-	 * The default values of the breakpoints can be configured using `require.config()`,
-	 * for instance:
-	 *
-	 * ```html
-	 * &lt;script>
-	 *   // configuring RequireJS
-	 *   require.config({
-	 *     ...
-	 *     config: {
-	 *       "deliteful/channelBreakpoints": {
-	 *         smallScreen: "280px",
-	 *         mediumScreen: "724px"
-	 *       }
-	 *     }
-	 *   });
-	 * &lt;/script>
-	 * ```
-	 *
 	 * @module deliteful/channelBreakpoints
 	 */
-	return /** @lends module:deliteful/channelBreakpoints# */ {
-		/**
-		 * The maximum screen size value for small screens.
-		 * Used as breakpoint by a CSS media query of `deliteful/features` as screen size
-		 * threshold between the phone-like and the tablet-like channels.
-		 * @member {string}
-		 * @default "480px"
-		 */
-		smallScreen: config.smallScreen || "480px",
 
-		/**
-		 * The maximum screen size value for medium screens.
-		 * Used as breakpoint by a CSS media query of `deliteful/features` as screen size
-		 * threshold between the tablet-like and the desktop-like channels.
-		 * @member {string}
-		 * @default "1024px"
-		 */
-		mediumScreen: config.mediumScreen || "1024px"
-	};
+	/**
+	 * The maximum screen size value for small screens.
+	 * Used as breakpoint by a CSS media query of `deliteful/features` as screen size
+	 * threshold between the phone-like and the tablet-like channels.
+	 * @member {string}
+	 * @default "480px"
+	 */
+	smallScreen: "480px",
+
+	/**
+	 * The maximum screen size value for medium screens.
+	 * Used as breakpoint by a CSS media query of `deliteful/features` as screen size
+	 * threshold between the tablet-like and the desktop-like channels.
+	 * @member {string}
+	 * @default "1024px"
+	 */
+	mediumScreen: "1024px"
 });
--- tests/unit/channelBreakpoints.js
+++ tests/unit/channelBreakpoints.js
@@ -12,27 +12,6 @@ define(function () {
 				assert.strictEqual(channelBreakpoints.smallScreen, "480px", "default value of smallScreen");
 				assert.strictEqual(channelBreakpoints.mediumScreen, "1024px", "default value of mediumScreen");
 			}));
-		},
-
-		"custom config": function () {
-			var configRequire = requirejs.config({
-				context: "configTest",
-				// baseUrl is relative to deliteful/node_modules/intern/
-				baseUrl: "../../..",
-				config: {
-					"deliteful/channelBreakpoints": {
-						smallScreen: "314px",
-						mediumScreen: "514px"
-					}
-				}
-			});
-
-			var dfd = this.async();
-
-			configRequire(["deliteful/channelBreakpoints"], dfd.callback(function (channelBreakpoints) {
-				assert.strictEqual(channelBreakpoints.smallScreen, "314px", "custom config of smallScreen");
-				assert.strictEqual(channelBreakpoints.mediumScreen, "514px", "custom config of mediumScreen");
-			}));
 		}
 	});
 });
